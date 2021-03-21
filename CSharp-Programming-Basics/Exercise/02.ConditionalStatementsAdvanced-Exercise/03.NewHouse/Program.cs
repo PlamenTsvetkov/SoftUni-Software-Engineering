1de84e4645fdc5d360800441e6196ba8d29c00e1
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-03-21 10:32:11 +0200

    Added new problem from the exercise
    New house

---
+++ CSharp-Programming-Basics/Exercise/02.ConditionalStatementsAdvanced-Exercise/03.NewHouse/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace _03.NewHouse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            double quantity = double.Parse(Console.ReadLine());
+            double budget = double.Parse(Console.ReadLine());
+
+            double totalSumFlawer = 0.00;
+            double price = 0.00;
+
+            if (type == "Roses")
+            {
+                price = 5;
+                if (quantity >80)
+                {
+                    totalSumFlawer = quantity * price * 0.9;
+                }
+                else
+                {
+                    totalSumFlawer = quantity * price;
+                }
+            }
+            else if (type == "Dahlias")
+            {
+                price = 3.80;
+                if (quantity > 90)
+                {
+                    totalSumFlawer = quantity * price * 0.85;
+                }
+                else
+                {
+                    totalSumFlawer = quantity * price;
+                }
+            }
+            else if (type == "Tulips")
+            {
+                price = 2.80;
+                if (quantity > 80)
+                {
+                    totalSumFlawer = quantity * price * 0.85;
+                }
+                else
+                {
+                    totalSumFlawer = quantity * price;
+                }
+            }
+            else if (type == "Narcissus")
+            {
+                price = 3.00;
+                if (quantity < 120)
+                {
+                    totalSumFlawer = quantity * price * 1.15;
+                }
+                else
+                {
+                    totalSumFlawer = quantity * price;
+                }
+            }
+            else if (type == "Gladiolus")
+            {
+                price = 2.50;
+                if (quantity < 80)
+                {
+                    totalSumFlawer = quantity * price * 1.20;
+                }
+                else
+                {
+                    totalSumFlawer = quantity * price;
+                }
+            }
+           
+            
+            if (budget-totalSumFlawer > 0)
+            {
+                Console.WriteLine($"Hey, you have a great garden with {quantity} {type} and {budget - totalSumFlawer:F2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money, you need { Math.Abs(budget - totalSumFlawer):F2} leva more.");   
+            }            
+        }
+    }
+}
