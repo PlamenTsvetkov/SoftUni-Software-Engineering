7e0aa7efd4adf17ee75ba83fbf3e042fe9306ac5
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-03-21 10:26:43 +0200

    Added new problem from exercise
    Cinema

---
+++ CSharp-Programming-Basics/Exercise/02.ConditionalStatementsAdvanced-Exercise/01Cinema/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine().ToLower();
+            double rows = double.Parse(Console.ReadLine());
+            double column = double.Parse(Console.ReadLine());
+
+            double totalChair = rows * column;
+            double price = 0.00;
+            
+
+            if (type =="premiere")
+            {
+                price = 12.00;
+                double totalSum = totalChair * price;
+                Console.WriteLine($"{totalSum:F2} leva");
+            }
+            else if (type == "normal")
+            {
+                price = 7.50;
+                double totalSum = totalChair * price;
+                Console.WriteLine($"{totalSum:F2} leva");
+            }
+            else if (type == "discount")
+            {
+                price = 5.00;
+                double totalSum = totalChair * price;
+                Console.WriteLine($"{totalSum:F2} leva");
+            }
+        }
+    }
+}
