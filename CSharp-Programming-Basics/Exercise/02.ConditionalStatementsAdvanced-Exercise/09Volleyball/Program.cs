1bf193a504be1ec260307ef4f5e398e77d4f1d84
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-03-23 11:18:08 +0200

    Added new problem from the exercise
    Volleyball

---
+++ CSharp-Programming-Basics/Exercise/02.ConditionalStatementsAdvanced-Exercise/09Volleyball/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _9Volleyball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string year = Console.ReadLine().ToLower();
+            double presents = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            double weekend = 48 - h;
+            double weekendPlay = weekend * 3 / 4;
+            double presentPlay =presents * 2 / 3;
+
+            double plays = weekendPlay + presentPlay + h;
+
+            if (year == "normal")
+            {
+                Console.WriteLine(Math.Floor(plays)); 
+            }
+            else if (year=="leap")
+            {
+                Console.WriteLine(Math.Floor(plays*1.15));
+            }
+        }
+    }
+}
