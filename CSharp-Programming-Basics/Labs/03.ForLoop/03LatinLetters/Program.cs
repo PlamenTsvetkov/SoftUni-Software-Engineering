7957b1af673f7896dc7f58aa0a0618be8c90f92f
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-03-23 11:23:46 +0200

    Added new problem from the exercise
    Latin Letters

---
+++ CSharp-Programming-Basics/Labs/03.ForLoop/03LatinLetters/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03LatinLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Азбуката е :");
+            for (var letter = 'a'; letter <= 'z'; letter++)
+            {
+
+                Console.Write(" " + letter);
+            }
+        }
+    }
+}
