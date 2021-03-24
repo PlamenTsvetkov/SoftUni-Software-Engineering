2543b66dccc97a757c04fe91672346352ea3d77b
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-03-24 09:54:59 +0200

    Added new problem from the exercise
    Sum numbers

---
+++ CSharp-Programming-Basics/Labs/03.ForLoop/04SumNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04NakovSumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Моля въведете брой числа н = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Моля въведете {0} числа едно след друго, всяко на отделен ред", n);
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("num {0} = " , i);
+                int num = int.Parse(Console.ReadLine());
+                sum = sum + num;
+            }
+            Console.WriteLine("Сумате е " + sum);
+        }
+    }
+}
