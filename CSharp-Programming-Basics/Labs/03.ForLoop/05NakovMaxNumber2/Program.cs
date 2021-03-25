83213a2ecc74d464cd30746f805ee917e8a2d45a
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-03-25 09:58:55 +0200

    Added new problem from the exercise
    Max number 2

---
+++ CSharp-Programming-Basics/Labs/03.ForLoop/05NakovMaxNumber2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05NakovMaxNumber2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int maxNum = int.MinValue;
+            int minNum = int.MaxValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                int current = int.Parse(Console.ReadLine());
+                if (current> maxNum)
+                {
+                    maxNum = current;
+                }
+                if (current <minNum)
+                {
+                    minNum = current;
+                }
+            }
+        }
+    }
+}
