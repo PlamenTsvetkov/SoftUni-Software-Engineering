2baec7a71ab78084dacda20e8fd141c90369ec30
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-03-25 09:57:43 +0200

    Added new problem from the exercise
    Max number

---
+++ CSharp-Programming-Basics/Labs/03.ForLoop/05NakovMaxNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _05NakovMaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Напишете N на брой числа :");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Напиши максимална сума :");
+            int maxNum = int.MinValue;
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("num {0} =", i);
+                int num = int.Parse(Console.ReadLine());
+                if (num > maxNum)
+                {
+                    maxNum = num;
+                }
+            }
+            Console.WriteLine("Max =" + maxNum);
+        }
+    }
+}
