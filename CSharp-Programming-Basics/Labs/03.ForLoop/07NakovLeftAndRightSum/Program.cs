92f0a1f9054015a3e676b02a051e80ddc0f21f0f
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-03-25 10:02:00 +0200

    Added new problem from the exercise
    Left and right sum

---
+++ CSharp-Programming-Basics/Labs/03.ForLoop/07NakovLeftAndRightSum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _07NakovLeftAndRightSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведете n числа: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int value1 = 0;
+            int value2 = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("num {0} = ",i);
+                int num1 = int.Parse(Console.ReadLine());
+                value1 += num1;
+            }
+            for (int x = 1; x <= n; x++)
+            {
+                Console.Write("num {0} = ", x);
+                int num2 = int.Parse(Console.ReadLine());
+                value2 += num2;
+            }
+            double difference = value1 - value2;
+            if (value1==value2)
+            {
+                Console.WriteLine("Yes, sum = " + value1);
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(difference)}");
+            }
+        }
+    }
+}
