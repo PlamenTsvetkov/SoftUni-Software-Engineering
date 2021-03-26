23ef163776d7a2c0a79e3547af8f7f78b7276f51
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-03-26 10:52:56 +0200

    Added new problem from the exercise
    Vowels sum

---
+++ CSharp-Programming-Basics/Labs/03.ForLoop/09NakovVowelsSum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _09NakovVowelsSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Напишете дума по избор : ");
+            string word = Console.ReadLine();
+
+            int sumLetter = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                char letter = word[i];
+                switch (letter)
+                {
+                    case 'a':
+                        sumLetter += 1;
+                        break;
+                    case 'e':
+                        sumLetter += 2;
+                        break;
+                    case 'i':
+                        sumLetter += 3;
+                        break;
+                    case 'o':
+                        sumLetter += 4;
+                        break;
+                    case 'u':
+                        sumLetter += 5;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine(sumLetter);
+        }
+    }
+}
