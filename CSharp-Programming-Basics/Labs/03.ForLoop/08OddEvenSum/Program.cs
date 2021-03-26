78e76ea019abf9ef868208e9b14cdff0da5d944c
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-03-26 10:50:32 +0200

    Added new problem from the exercise
    Odd even sum

---
+++ CSharp-Programming-Basics/Labs/03.ForLoop/08OddEvenSum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _08OddEvenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведете n брой цели числа: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int oddSum = 0;
+            int evenSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("num {0} = ",i);
+                int num = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    oddSum += num;
+                }
+                else
+                {
+                    evenSum += num;
+                }        
+            }
+            int diff = Math.Abs(oddSum - evenSum);
+            if (oddSum == evenSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = "+oddSum);
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = "+diff);
+            }
+        }
+    }
+}
