6de43c1d8272d9a1d325a39a54a8ec89bddf5c29
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-03-26 10:49:52 +0200

    Added new problem from the exercise
    Number sequence

---
+++ CSharp-Programming-Basics/Labs/03.ForLoop/08NumberSequence/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _08NumberSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int minValue = int.MaxValue;
+            int maxValue = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                int value = int.Parse(Console.ReadLine());
+                if (value<minValue)
+                {
+                    minValue = value;
+                }
+                if (value> maxValue)
+                {
+                    maxValue = value;
+                }
+            }
+            Console.WriteLine($"Max number: {maxValue}");
+            Console.WriteLine($"Min number: {minValue}");
+        }
+    }
+}
