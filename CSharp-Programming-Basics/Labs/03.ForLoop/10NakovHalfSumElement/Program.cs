3c4741ffb9ba3e2be4b02cc5c79bdd54bbd2fa22
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-03-27 09:12:26 +0200

    Added new problem from the exercise
    Half sum element

---
+++ CSharp-Programming-Basics/Labs/03.ForLoop/10NakovHalfSumElement/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _10NakovHalfSumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Напишете n брой числа : ");
+            int n = int.Parse(Console.ReadLine());
+
+            int maxSum = int.MinValue;
+            int totalN = 0;
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("number {0} = ",i);
+                int num = int.Parse(Console.ReadLine());
+
+                totalN += num;
+                if (num > maxSum)
+                {
+                    maxSum = num;
+                }
+                
+            }
+
+            if (maxSum == totalN - maxSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + maxSum);
+            }
+            else
+            {
+                int diff = maxSum - (totalN -maxSum);
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(diff)}");
+            }
+        } 
+	}    
+}
