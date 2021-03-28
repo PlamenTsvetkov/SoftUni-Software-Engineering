efa4186b5c5803f772fa311686004b83d5606e9e
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-03-28 14:15:18 +0300

    Added new problem from the exercise
    Odd even possition

---
+++ CSharp-Programming-Basics/Exercise/03.ForLoop-Exercise/03OddEvenPossition/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace _03OddEvenPossition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double sumOdd = 0.00;
+            double minOdd = double.MaxValue;
+            double maxOdd = double.MinValue;
+            double sumEven = 0.00;
+            double minEven = double.MaxValue;
+            double maxEven = double.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i%2==0)
+                {
+                    double numOdd = double.Parse(Console.ReadLine());
+                    sumOdd += numOdd;
+                    if (minOdd>numOdd)
+                    {
+                        minOdd = numOdd;
+                    }
+                    if (maxOdd < numOdd)
+                    {
+                        maxOdd = numOdd;
+                    }
+                }
+                else
+                {
+                    double numEven = double.Parse(Console.ReadLine());
+                    sumEven += numEven;
+                    if (minEven > numEven)
+                    {
+                        minEven = numEven;
+                    }
+                    if (maxEven < numEven)
+                    {
+                        maxEven = numEven;
+                    }
+                }
+            }
+            Console.WriteLine($"OddSum={sumOdd:F2},");
+            if (minOdd== double.MaxValue)
+            {
+                Console.WriteLine("OddMin=No,");
+            }
+            else
+            {
+                Console.WriteLine($"OddMin={minOdd:F2},");
+            }
+            if (maxOdd == double.MinValue)
+            {
+                Console.WriteLine("OddMax=No,");
+            }
+            else
+            {
+                Console.WriteLine($"OddMax={maxOdd:F2},");
+            }
+            Console.WriteLine($"EvenSum={sumEven:F2},");
+            if (minEven == double.MaxValue)
+            {
+                Console.WriteLine("EvenMin=No,");
+            }
+            else
+            {
+                Console.WriteLine($"EvenMin={minEven:F2},");
+            }
+            if (maxEven == double.MinValue)
+            {
+                Console.WriteLine("EvenMax=No");
+            }
+            else
+            {
+                Console.WriteLine($"EvenMax={maxEven:F2}");
+            }
+
+        }
+    }
+}
