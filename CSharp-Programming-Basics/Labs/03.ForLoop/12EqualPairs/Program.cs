1b31dbf0dd9cbb123b84436f84bcea3640490ce1
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-03-28 14:05:22 +0300

    Added new problem from the exercise
    Equal pairs

---
+++ CSharp-Programming-Basics/Labs/03.ForLoop/12EqualPairs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _12EqualPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double firstSum = 0.00;
+            double lastSum = 0.00;
+            double diff = 0.00;
+
+
+            for (int i = 0; i < n ; i++)
+            {
+                double num1 = double.Parse(Console.ReadLine());
+                double num2 = double.Parse(Console.ReadLine());
+                firstSum = num1 + num2;
+                
+                if (i > 0)
+                {
+                    lastSum = firstSum;
+                    diff = Math.Max(lastSum, firstSum)- Math.Min(lastSum, firstSum);                    
+                }
+            }
+            if (diff==0)
+            {
+                Console.WriteLine($"Yes, value={firstSum}");
+            }
+            else
+            {
+                Console.WriteLine($"No maddiff={diff}");
+            }
+        }
+    }
+}
