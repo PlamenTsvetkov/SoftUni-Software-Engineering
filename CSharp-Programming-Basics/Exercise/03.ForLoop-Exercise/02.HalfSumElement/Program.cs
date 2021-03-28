e7bd392ad7c10b522a653d1b88faf4c1d0b4e7d3
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-03-28 14:13:50 +0300

    Added new problem from the exercise
    Half sum element

---
+++ CSharp-Programming-Basics/Exercise/03.ForLoop-Exercise/02.HalfSumElement/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _02.HalfSumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int numMax = int.MinValue;
+            int sum = 0;
+
+
+            for (int i = 0; i < n; i++)
+            {
+               int num = int.Parse(Console.ReadLine());
+                sum += num;
+                if (numMax < num)
+                {
+                    numMax = num;
+                }
+
+            }
+            int diff = sum - numMax;
+            int diff2 = numMax - (sum - numMax);
+            if (numMax == diff)
+            {
+                Console.WriteLine($"Yes");
+                Console.WriteLine($"Sum = {diff}");
+            }
+            else
+            {
+                Console.WriteLine($"No");
+                Console.WriteLine($"Diff = {Math.Abs(diff2)}");
+            }
+        }
+    }
+}
