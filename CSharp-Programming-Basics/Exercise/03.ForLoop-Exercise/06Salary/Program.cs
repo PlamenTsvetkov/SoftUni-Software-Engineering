836dab3f8e0fdf250614c60b263a266de2cb5585
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-03-29 09:52:58 +0300

    Added new problem from the exercise
    Salary

---
+++ CSharp-Programming-Basics/Exercise/03.ForLoop-Exercise/06Salary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _06Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tabs = double.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < tabs; i++)
+            {                
+                    string site = Console.ReadLine();
+                    if (site == "Facebook")
+                    {
+                        salary -= 150;                        
+                    }
+                    else if (site == "Instagram")
+                    {
+                        salary -= 100; 
+                    }
+                    else if (site == "Reddit")
+                    {
+                        salary -= 50;
+                    }
+            if  (salary <= 0)
+                {
+                    Console.WriteLine("You have lost your salary.");
+                    break;
+                }
+
+        }
+                if (salary>0)
+                {
+                    Console.WriteLine(salary);
+                }                   
+        }
+    }
+}
