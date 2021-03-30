4473f24b99ac47ef7bc15af0d3af2707ee55a594
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-03-30 09:52:12 +0300

    Added new problem from the exercise
    Password

---
+++ CSharp-Programming-Basics/Labs/04.WhileLoop/P2Password/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02AnaPassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            var pass = Console.ReadLine();
+            var input = Console.ReadLine();
+
+            while (input != pass)
+            {
+                input = Console.ReadLine();
+
+            }
+            Console.WriteLine($"Welcome {name}!");
+        }
+    }
+}
