c67fdbff3338fbbc2706e8589ac32201e561349b
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-03-30 09:52:52 +0300

    Added new problem from the exercise
    Sum numbers

---
+++ CSharp-Programming-Basics/Labs/04.WhileLoop/P3SumNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _03AnaSumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            while (true)
+            {
+                int b = int.Parse(Console.ReadLine());
+                sum += b;
+                if (sum>=a)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
