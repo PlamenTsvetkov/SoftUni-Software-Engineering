6abe76853f05e766aeec7e53ecaf53b4f75767e0
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-03-30 09:53:35 +0300

    Added new problem from the exercise
    Sequence 2k

---
+++ CSharp-Programming-Basics/Labs/04.WhileLoop/P4Sequence2k/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _04.AnaSequence2k_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            
+            int b = 1;
+            while (b <= a)
+            {
+                Console.WriteLine(b);
+                b = b * 2 + 1;
+            }
+        }
+    }
+}
