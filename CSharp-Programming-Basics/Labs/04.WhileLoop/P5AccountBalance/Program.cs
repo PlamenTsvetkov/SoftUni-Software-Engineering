cd1ef50769ee1c144eb40c9b5008d16c4d637f8e
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-03-30 09:54:14 +0300

    Added new problem from the exercise
    Account balance

---
+++ CSharp-Programming-Basics/Labs/04.WhileLoop/P5AccountBalance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05.AnaAccountBalanceTrue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double totalAmount = 0;
+
+            while (input != "NoMoreMoney")
+            {
+                double amount = double.Parse(input);
+                if (amount <0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                Console.WriteLine($"Increase: {amount:F2}");
+                totalAmount += amount;
+                input = Console.ReadLine(); 
+            }
+            Console.WriteLine($"Total: {totalAmount:F2}");
+
+        }
+    }
+}
