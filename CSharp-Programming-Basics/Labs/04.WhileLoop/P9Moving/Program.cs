45b4ac7a60428988feb19710607d3de8b2a3f726
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-03-31 14:26:58 +0300

    Added new problem from the exercise
    Moving

---
+++ CSharp-Programming-Basics/Labs/04.WhileLoop/P9Moving/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _09.AnaMoving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double lenght = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            string num = Console.ReadLine();
+
+            double  totalBox = 0.00;
+
+            double cubik = width * lenght * height;
+
+            while (num != "Done")
+            {
+                int box = int.Parse(num);
+                totalBox += box;
+                if (cubik <= totalBox)
+                {
+                    Console.WriteLine($"No more free space! You need {totalBox-cubik} Cubic meters more.");
+                    break;
+                }
+                num = Console.ReadLine(); 
+            }
+            if (cubik > totalBox)
+            {
+                Console.WriteLine($"{cubik-totalBox} Cubic meters left.");
+            }
+        }
+    }
+}
