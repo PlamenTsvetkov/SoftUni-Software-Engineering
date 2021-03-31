be53babb5935b13b4fae27564b37f7bc2c7f98a1
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-03-31 14:24:43 +0300

    Added new problem from the exercise
    Max number

---
+++ CSharp-Programming-Basics/Labs/04.WhileLoop/P6MaxNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _06MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int maxNum = int.MinValue;
+
+            while (input != "Stop")
+            {
+                 int number = int.Parse(input);
+                if (number>maxNum)
+                {
+                    maxNum = number;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(maxNum);
+        }
+    }
+}
