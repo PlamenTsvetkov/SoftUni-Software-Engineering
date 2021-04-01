0e20e1f9dcb52ade45b3c73f38591625c6b9c5ea
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-01 18:30:17 +0300

    Added new problem from the exercise
    Old book

---
+++ CSharp-Programming-Basics/Exercise/04.WhileLoop-Exercise/P01OldBooks/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01.AnaOldBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bookName = Console.ReadLine();
+
+            string books = Console.ReadLine();
+            int search = 0;
+
+            while (books != "No More Books")
+            {
+                if (bookName == books)
+                {
+                    Console.WriteLine($"You checked {search} books and found it.");
+                    break;
+                }
+                search += 1;
+                books = Console.ReadLine();
+            }
+            if (books == "No More Books")
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {search} books.");
+            }
+        }
+    }
+}
