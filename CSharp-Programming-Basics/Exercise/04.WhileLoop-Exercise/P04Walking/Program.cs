4f10302c350fe468677562fa068a470c17a46017
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-01 18:32:33 +0300

    Added new problem from the exercise
    Walking

---
+++ CSharp-Programming-Basics/Exercise/04.WhileLoop-Exercise/P04Walking/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _04.AnaWalking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int totalSteps = 0;
+            int stepsToHome = 0;
+            int goal = 10000;
+            
+            while (input!="Going home")
+            {
+                
+                int steps = int.Parse(input);
+                totalSteps += steps;
+                if (totalSteps>=goal)
+                {
+                    break;
+                }
+                input = Console.ReadLine();
+            }
+            if ((input == "Going home"))
+            {
+                stepsToHome = int.Parse(Console.ReadLine());
+                totalSteps += stepsToHome;
+            }            
+            if (totalSteps>=goal)
+            {
+                Console.WriteLine($"Goal reached! Good job!");
+                Console.WriteLine($"{totalSteps - goal} steps over the goal!");
+            }
+            else
+            {
+                Console.WriteLine($"{goal-totalSteps} more steps to reach goal.");
+            }            
+        }
+    }
+}
