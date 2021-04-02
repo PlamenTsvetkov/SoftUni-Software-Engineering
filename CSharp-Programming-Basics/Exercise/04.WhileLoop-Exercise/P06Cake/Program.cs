3ead713aed000f0d983c824d73e3523f6a5c340d
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-02 09:27:01 +0300

    Added new problem from the exercise
    Cake

---
+++ CSharp-Programming-Basics/Exercise/04.WhileLoop-Exercise/P06Cake/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _06.AnaCake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+            
+            string sumCakeString = Console.ReadLine();
+            int totalSumCake = 0;
+            int totalCake = width * lenght;
+            while (sumCakeString != "STOP")
+            {
+                int sumCake = int.Parse(sumCakeString);
+                totalSumCake += sumCake;
+                if (totalSumCake >= totalCake)
+                {
+                    break;
+                }
+                sumCakeString = Console.ReadLine();
+            }
+            int cakeLeft = totalCake - totalSumCake;
+            int moreCake = totalSumCake - totalCake;
+            if (sumCakeString == "STOP")
+            {
+                Console.WriteLine($"{cakeLeft} pieces are left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more cake left! You need {moreCake} pieces more.");
+            }
+
+            
+        }
+    }
+}
