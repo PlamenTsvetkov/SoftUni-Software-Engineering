87852283de228c471f51d47295abebb7e1e88d86
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-03 11:06:48 +0300

    Added new problem from the exercise
    Traveling

---
+++ CSharp-Programming-Basics/Labs/05.NastedLoop-Lab/05.Traveling/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05.AnaTraveling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            double money = 0.00;
+
+            double saveMoney = 0.00;
+
+            while (true)
+            {
+                string destination = Console.ReadLine();
+                if (destination == "End")
+                {
+                    break;
+                }
+                double minBudget = double.Parse(Console.ReadLine());
+                while (saveMoney<minBudget)
+                {
+                     money = double.Parse(Console.ReadLine());
+                    saveMoney += money;
+                }
+                Console.WriteLine($"Going to { destination}!");
+                saveMoney = 0;
+            }
+            
+        }
+    }
+}
