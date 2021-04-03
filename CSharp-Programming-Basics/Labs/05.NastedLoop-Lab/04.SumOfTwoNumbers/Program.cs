da9c18f81e25a169ea18e895ed0c617ad0bc79ec
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-03 11:06:04 +0300

    Added new problem from the exercise
    Sum of two numbers

---
+++ CSharp-Programming-Basics/Labs/05.NastedLoop-Lab/04.SumOfTwoNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _04.AnaSumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int finish = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            bool magicNumberFound = false;
+
+            int combination = 0;
+            for (int i = start; i <= finish; i++)
+            {
+                for (int j = start; j <= finish; j++)
+                {
+                    combination++;
+                    int result = i + j;
+                    if (result==magicNumber)
+                    {
+                        Console.WriteLine($"Combination N:{combination} ({i} + {j} = {magicNumber})");
+                        magicNumberFound = true;
+                        break;
+                    }
+                }
+                if (magicNumberFound)
+                {
+                    break;
+                }
+            }
+            if (!magicNumberFound)
+            {
+                Console.WriteLine($"{combination} combinations - neither equals { magicNumber }");
+            }            
+        }
+    }
+}
