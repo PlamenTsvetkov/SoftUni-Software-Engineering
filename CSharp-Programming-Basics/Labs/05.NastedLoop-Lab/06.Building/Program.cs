4b98d9424e36c44bfd28e87125f72824acfa9295
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-04 09:57:43 +0300

    Added new problem from the exercise
    Building

---
+++ CSharp-Programming-Basics/Labs/05.NastedLoop-Lab/06.Building/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _06.AnaBuilding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countFloor = int.Parse(Console.ReadLine());
+            int countRooms = int.Parse(Console.ReadLine());
+
+            for (int f = countFloor; f >= 1; f--)
+            {
+                for (int r = 0; r < countRooms; r++)
+                {
+                    if (f==countFloor)
+                    {
+                        Console.Write($"L{f}{r} ");
+                    }
+                    else
+                    {
+                        if (f % 2 == 0)
+                        {
+                            Console.Write($"O{f}{r} ");
+                        }
+                        else
+                        {
+                            Console.Write($"A{f}{r} ");
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
