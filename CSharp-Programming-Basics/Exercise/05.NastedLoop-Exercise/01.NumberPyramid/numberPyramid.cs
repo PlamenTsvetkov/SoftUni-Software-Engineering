dd518c862b335e9075491b6ba188429ea211176e
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-05 08:11:27 +0300

    Added new problem from the exercise
    Pyramid

---
+++ CSharp-Programming-Basics/Exercise/05.NastedLoop-Exercise/01.NumberPyramid/numberPyramid.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01NumberPyramid
+{
+    class numberPyramid
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int current = 1;
+            bool isBigger = false;
+            for (int i = 1; i <=n; i++)
+            {
+                for (int colm = 0; colm < i; colm++)
+                {
+                    if (current>n)
+                    {
+                        isBigger = true;
+                        break;
+                    }
+                    Console.Write(current+" ");
+                    current++;
+                }
+                if (isBigger)
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
