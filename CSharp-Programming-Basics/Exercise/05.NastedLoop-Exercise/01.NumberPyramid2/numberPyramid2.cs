f14830ca72d7f1653099e92e5dffdb16681c0a94
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-05 08:14:20 +0300

    Added new problem from the exercise
    Second option

---
+++ CSharp-Programming-Basics/Exercise/05.NastedLoop-Exercise/01.NumberPyramid2/numberPyramid2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01numberPyramid2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int maxColumn = 0;
+            bool flag = false;
+            for (int rows = 1; rows <= n; rows++)
+            {
+                for (int column = 1; column <= rows; column++)
+                {
+                    maxColumn++;
+                    Console.Write($"{maxColumn} ");
+                    if (maxColumn==n)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag)
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
