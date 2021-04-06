4f3ed54e8d8562b4461bfcf5d7e961b5e32db49c
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-06 09:42:34 +0300

    Added new problem from the exercise
    Equal sums even odd position

---
+++ CSharp-Programming-Basics/Exercise/05.NastedLoop-Exercise/02.qualSumsEvenOddPosition/equalSumsEvenOddPosition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _02equalSumsEvenOddPosition
+{
+    class equalSumsEvenOddPosition
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+
+            
+            for (int i = n1; i <= n2; i++)
+            {
+                string currentNum = i.ToString();
+                int oddSum = 0;
+                int evenSum = 0;
+                for (int j = 0; j < currentNum.Length; j++)
+                {
+                    int currentDiggit = int.Parse(currentNum[j].ToString());
+                    if (j % 2 == 0)
+                    {
+                        evenSum += currentDiggit;
+                    }
+                    else
+                    {
+                        oddSum += currentDiggit;
+                    }
+                }
+                if (oddSum==evenSum)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
