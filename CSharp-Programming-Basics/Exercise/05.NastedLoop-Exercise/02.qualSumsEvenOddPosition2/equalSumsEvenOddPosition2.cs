13176b44b28669fde174249aa15a281325fd9333
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-06 09:45:12 +0300

    Added new problem from the exercise
    Second option

---
+++ CSharp-Programming-Basics/Exercise/05.NastedLoop-Exercise/02.qualSumsEvenOddPosition2/equalSumsEvenOddPosition2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _02equalSumsEvenOddPosition2
+{
+    class equalSumsEvenOddPosition2
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            
+            for (int i = number1; i <= number2; i++)
+            {
+                int totalOdd = 0;
+                int totalEven = 0;
+                string stringNumber = i.ToString();
+                for (int j = 0; j < stringNumber.Length; j++)
+                {
+                    int result = int.Parse(stringNumber[j].ToString());
+                    if (j%2==0)
+                    {
+
+                        totalEven += result;
+                    }
+
+                    if (j % 2 == 1)
+                    {
+                        totalOdd += result;
+                    }
+                }
+                if (totalEven==totalOdd)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
