18dcd797946dfe55c92f0985db4239b92418dda6
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-07 09:49:43 +0300

    Added new problem from the exercise
    Train the trainers

---
+++ CSharp-Programming-Basics/Exercise/05.NastedLoop-Exercise/04.TrainTheTrainers/trainTheTrainers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _04TrainTheTrainers
+{
+    class trainTheTrainers
+    {
+        static void Main(string[] args)
+        {
+            double jury = double.Parse(Console.ReadLine());
+            string presentationName = "";
+            double counter = 0;
+            double absoluteGrade = 0;
+            while ((presentationName = Console.ReadLine())!= "Finish")
+            {
+                double totalGrade = 0;
+                for (int i = 0; i < jury; i++)
+                {
+                    double grade = double.Parse(Console.ReadLine());
+                    totalGrade += grade;
+                    counter++;
+                    absoluteGrade += grade;
+                }
+                Console.WriteLine($"{presentationName} - {totalGrade / jury:f2}.");
+            }
+            Console.WriteLine($"Student's final assessment is {absoluteGrade/counter:f2}.");
+        }
+    }
+}
