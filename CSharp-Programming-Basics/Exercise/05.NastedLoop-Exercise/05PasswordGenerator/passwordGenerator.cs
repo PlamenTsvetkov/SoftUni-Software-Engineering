0baee306cbcc7f56b929e2ccdb4c9baa9d0fbb6b
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-08 09:49:05 +0300

    Added new problem from the exercise
    Generator

---
+++ CSharp-Programming-Basics/Exercise/05.NastedLoop-Exercise/05PasswordGenerator/passwordGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _05PasswordGenerator
+{
+    class passwordGenerator
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            double l = double.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    for (int k = 'a'; k < 'a'+ l; k++)
+                    {
+                        for (int m = 'a'; m <'a'+ l; m++)
+                        {
+                            for (int o = 2; o <= n; o++)
+                            {
+                                if (o>i && o>j)
+                                {
+                                    Console.Write($"{i}{j}{(char)k}{(char)m}{o} ");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
