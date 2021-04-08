a9fff787cf7f6f81bb5f1d18a11b5c5f0299252c
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-08 09:50:51 +0300

    Added new problem from the exercise
    Special numbers

---
+++ CSharp-Programming-Basics/Exercise/05.NastedLoop-Exercise/06specialNumbers/specialNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06specialNumbers
+{
+    class specialNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int ifTrue = 0;
+            for (int i = 1111; i <= 9999; i++)
+            {
+                int numi = i;
+                for (int j = 1; j <= 4; j++)
+                {
+                    if (numi % 10 == 0)
+                    {
+                        break;
+                    }
+                    if (n % (numi % 10) == 0)
+                    {
+                        ifTrue += 1;
+                    }
+                    numi = numi / 10;
+                    
+                }
+                if (ifTrue == 4)
+                {
+                    Console.Write($"{i} ");
+                }
+                ifTrue = 0;
+
+            }
+
+        }
+    }
+}
