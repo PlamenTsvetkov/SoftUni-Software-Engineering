6054d898090f75cbe934f1530c7d294875e89781
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-09 09:14:51 +0300

    Added new problem from the exam
    Safari

---
+++ CSharp-Programming-Basics/Exams/e1.Exam02-03.05.2019/02.Safari/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _02.Safari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            double fuelL = double.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+            double fuelPrice = 2.10;
+            double fuelTotalPrice = fuelL * fuelPrice;            
+            double gidPrice = 100;
+            double totalSum = fuelTotalPrice +gidPrice;
+            if (day == "Saturday")
+            {
+                totalSum = totalSum * 0.90;
+            }
+            else if (day == "Sunday")
+            {
+                totalSum = totalSum * 0.80;
+            }
+            if (budget>=totalSum)
+            {
+                Console.WriteLine($"Safari time! Money left: {budget-totalSum:f2} lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! Money needed: {totalSum-budget:f2} lv.");
+            }
+
+        }
+    }
+}
