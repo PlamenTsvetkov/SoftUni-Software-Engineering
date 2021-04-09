4c612763e7194de754830c4ded841246c264639d
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-09 09:12:46 +0300

    Added new problem from the exam
    Food exam

---
+++ CSharp-Programming-Basics/Exams/e1.Exam02-03.05.2019/01.FoodDelivery/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01.FoodDelivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chickenQ = int.Parse(Console.ReadLine());
+            int fishQ = int.Parse(Console.ReadLine());
+            int veganQ = int.Parse(Console.ReadLine());
+
+            double chickenPrice = 10.35;
+            double fishPrice = 12.40;
+            double veganPrice = 8.15;
+            double startTotalPrice = chickenPrice * chickenQ + fishPrice * fishQ + veganPrice * veganQ;
+            double priceDesert = startTotalPrice * 0.2;
+            double diliveryPrice = 2.50;
+            double finishTotalPrice = startTotalPrice + priceDesert + diliveryPrice;
+            Console.WriteLine($"Total: {finishTotalPrice:F2}");
+
+        }
+    }
+}
