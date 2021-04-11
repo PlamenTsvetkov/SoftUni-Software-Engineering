b0e891707eb883469a440063898ee873c4344df9
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-11 14:16:29 +0300

    Added new problem from the exam
    Vet parking

---
+++ CSharp-Programming-Basics/Exams/e1.Exam02-03.05.2019/06.VetParking/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06.VetParking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double days = double.Parse(Console.ReadLine());
+            double hours = double.Parse(Console.ReadLine());
+            
+            double totalSum = 0.00;
+            for (int iday = 1; iday <= days; iday++)
+            {
+                double sumForDay = 0.00;
+                for (int ihour = 1; ihour <= hours; ihour++)
+                {
+                    if (iday%2==0 && ihour%2!=0)
+                    {
+                        sumForDay += 2.50;
+                    }
+                    else if (iday % 2 != 0 && ihour % 2 == 0)
+                    {
+                        sumForDay += 1.25;
+                    }
+                    else
+                    {
+                        sumForDay += 1;
+                    }
+                }
+                totalSum += sumForDay;
+                Console.WriteLine($"Day: {iday} - {sumForDay:F2} leva");
+            }
+            Console.WriteLine($"Total: {totalSum:F2} leva");
+        }
+    }
+}
