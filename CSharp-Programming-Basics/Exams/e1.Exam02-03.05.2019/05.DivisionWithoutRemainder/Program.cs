f37fd051687c5ffe1c7e9eab4718b3d082b1dce2
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-11 14:14:19 +0300

    Added new problem from the exam
    Division without remainder

---
+++ CSharp-Programming-Basics/Exams/e1.Exam02-03.05.2019/05.DivisionWithoutRemainder/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05.DivisionWithoutRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            double p1 = 0;
+            double p2 = 0;
+            double p3 = 0;
+            for (int i = 0; i < n; i++)
+            {
+              int num = int.Parse(Console.ReadLine());
+                if (num%2==0)
+                {
+                    p1 += 1;
+                }
+                if (num%3==0)
+                {
+                    p2 += 1;
+                }
+                if (num%4==0)
+                {
+                    p3 += 1;
+                }
+            }
+            Console.WriteLine($"{p1/n*100:F2}%");
+            Console.WriteLine($"{p2/n*100:F2}%");
+            Console.WriteLine($"{p3/n*100:F2}%");
+        }
+    }
+}
