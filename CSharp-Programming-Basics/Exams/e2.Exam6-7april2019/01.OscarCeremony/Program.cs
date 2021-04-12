b1a6b9fc8753183bd899286f81375468f1f3c4d9
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-12 09:35:16 +0300

    Added new problem from the exam
    Oscar ceremony

---
+++ CSharp-Programming-Basics/Exams/e2.Exam6-7april2019/01.OscarCeremony/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01.OscarCeremony
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rent = double.Parse(Console.ReadLine());
+
+            double statue = rent * 0.70;
+            double catering = statue * 0.85;
+            double sound = catering * 1 / 2;
+            double totalExpenses = rent + statue + catering + sound;
+            Console.WriteLine($"{ totalExpenses:F2}");
+           
+        }
+    }
+}
