ef6d6e18cc3aaca0ae00ceca170dcfc0c5f5a320
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-13 09:20:08 +0300

    Added new problem from the exam
    Oscar week in cinema

---
+++ CSharp-Programming-Basics/Exams/e2.Exam6-7april2019/03.OscarWeekInCinema/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace _03.OscarWeekInCinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameFilm = Console.ReadLine();   
+            string type = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            double filmPrice = 0.00;
+
+            if (nameFilm=="A Star Is Born")
+            {
+                if (type=="normal")
+                {
+                    filmPrice = 7.50;
+                }
+                else if (type == "luxury")
+                {
+                    filmPrice = 10.50;
+                }
+                else if (type == "ultra luxury")
+                {
+                    filmPrice = 13.50;
+                }
+            }
+            else if (nameFilm == "Bohemian Rhapsody")
+            {
+                if (type == "normal")
+                {
+                    filmPrice = 7.35;
+                }
+                else if (type == "luxury")
+                {
+                    filmPrice = 9.45;
+                }
+                else if (type == "ultra luxury")
+                {
+                    filmPrice = 12.75;
+                }
+            }
+            else if (nameFilm == "Green Book")
+            {
+                if (type == "normal")
+                {
+                    filmPrice = 8.15;
+                }
+                else if (type == "luxury")
+                {
+                    filmPrice = 10.25;
+                }
+                else if (type == "ultra luxury")
+                {
+                    filmPrice = 13.25;
+                }
+            }
+            else if (nameFilm == "The Favourite")
+            {
+                if (type == "normal")
+                {
+                    filmPrice = 8.75;
+                }
+                else if (type == "luxury")
+                {
+                    filmPrice = 11.55;
+                }
+                else if (type == "ultra luxury")
+                {
+                    filmPrice = 13.95;
+                }
+            }
+            double totalIncome = quantity * filmPrice;
+            Console.WriteLine($"{nameFilm} -> {totalIncome:F2} lv.");
+        }
+    }
+}
