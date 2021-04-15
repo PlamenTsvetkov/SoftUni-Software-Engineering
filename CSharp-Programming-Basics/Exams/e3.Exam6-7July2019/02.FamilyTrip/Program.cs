c9981e91c899f80799efae19445c63b04f1ecf4d
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-15 09:36:11 +0300

    Added new problem from the exam
    Family trip

---
+++ CSharp-Programming-Basics/Exams/e3.Exam6-7July2019/02.FamilyTrip/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _02.FamilyTrip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int nights = int.Parse(Console.ReadLine());
+            double nightPrice = double.Parse(Console.ReadLine());
+            int percentExpenses = int.Parse(Console.ReadLine());
+
+            if (nights>7)
+            {
+                nightPrice = nightPrice * 0.95;
+            }
+            double additionalCosts = budget * percentExpenses / 100;
+            double totalExpenses = (nights * nightPrice) + additionalCosts;
+            if (budget>= totalExpenses)
+            {
+                Console.WriteLine($"Ivanovi will be left with {budget-totalExpenses:F2} leva after vacation.");
+            }
+            else
+            {
+                Console.WriteLine($"{totalExpenses-budget:F2} leva needed.");
+            }
+        }
+    }
+}
