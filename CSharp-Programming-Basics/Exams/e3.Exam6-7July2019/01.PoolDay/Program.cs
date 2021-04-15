acfcf797bf2d87ca7f22ea3db1ca4faa2a4a59ef
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-15 09:34:51 +0300

    Added new problem from the exam
    Pool day

---
+++ CSharp-Programming-Basics/Exams/e3.Exam6-7July2019/01.PoolDay/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01.PoolDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleNum = int.Parse(Console.ReadLine());
+            double fee = double.Parse(Console.ReadLine());
+            double deskChairPrice = double.Parse(Console.ReadLine());
+            double umbrellaPrice = double.Parse(Console.ReadLine());
+
+            double totalUmbrella = Math.Ceiling(peopleNum / 2.0);
+            double totalDeskChair = Math.Ceiling(peopleNum * 0.75);
+
+
+            double totalFee = peopleNum * fee;
+            double totalUmbrellaPrice =  totalUmbrella* umbrellaPrice;
+            double totalDeskChairPrice = totalDeskChair * deskChairPrice;
+
+            double totalSum = totalFee + totalUmbrellaPrice + totalDeskChairPrice;
+            Console.WriteLine($"{totalSum:F2} lv.");
+
+        }
+    }
+}
