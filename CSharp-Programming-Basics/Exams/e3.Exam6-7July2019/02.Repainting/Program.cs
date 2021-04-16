852a0fb6546646c5f19fcf8fa8cfd2a3db2d2544
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-16 16:58:47 +0300

    Added new problem from the exam
    Repainting

---
+++ CSharp-Programming-Basics/Exams/e3.Exam6-7July2019/02.Repainting/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _02.Repainting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nailon = int.Parse(Console.ReadLine());
+            int paint = int.Parse(Console.ReadLine());
+            int razreditel = int.Parse(Console.ReadLine());
+            int hourWork = int.Parse(Console.ReadLine());
+
+            double totalPaintPrice = paint * 1.10 * 14.50;
+            double totalNailonPrice = (nailon + 2) * 1.50;
+            double totalRazreditelPrice = razreditel * 5;
+
+            double totalExpensesForMaterial = totalPaintPrice + totalNailonPrice + totalRazreditelPrice + 0.40;
+            double totalPriceWork = totalExpensesForMaterial * 0.30*hourWork;
+            double total = totalExpensesForMaterial + totalPriceWork;
+            Console.WriteLine($"Total expenses: {total:F2} lv.");
+
+        }
+    }
+}
