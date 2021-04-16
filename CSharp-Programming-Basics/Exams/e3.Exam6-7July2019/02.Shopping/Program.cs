3c5f7d1ad26dc3576923c53b1453b3e8589c0b1c
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-16 17:00:07 +0300

    Added new problem from the exam
    Shopping

---
+++ CSharp-Programming-Basics/Exams/e3.Exam6-7July2019/02.Shopping/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _02.Shopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int videocards = int.Parse(Console.ReadLine());
+            int procesors = int.Parse(Console.ReadLine());
+            int ram = int.Parse(Console.ReadLine());
+            
+            double videocardsPrice = 250;
+            double totalSum = 0;
+
+            
+            double videocardsSum = videocards * videocardsPrice;
+            double procesorsSum = procesors * (videocardsSum* 0.35);
+            double ramSum = ram * (videocardsSum * 0.10);
+            if (videocards > procesors)
+            {
+                 totalSum = (videocardsSum + procesorsSum + ramSum) * 0.85;
+            }
+            else
+            {
+                totalSum = (videocardsSum + procesorsSum + ramSum);
+            }
+
+            if (budget >= totalSum)
+            {
+                Console.WriteLine($"You have {budget - totalSum:F2} leva left!");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {Math.Abs(totalSum - budget):F2} leva more!");
+            }
+
+        }
+    }
+}
