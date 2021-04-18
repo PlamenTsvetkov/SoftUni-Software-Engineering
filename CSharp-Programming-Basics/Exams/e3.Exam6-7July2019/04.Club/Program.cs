015a31e981b56bda72a69b22d2b1051e97bb0683
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-18 14:37:43 +0300

    Added new problem from the exam
    Club

---
+++ CSharp-Programming-Basics/Exams/e3.Exam6-7July2019/04.Club/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _04.Club
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double profit = double.Parse(Console.ReadLine());
+            string nameCocktail = "";
+            int numCocktail = 0;
+
+
+            int price = 0;
+            double totalPrice = 0.00;
+            double allSum = 0.00;
+            while (profit>allSum)
+            {
+                 nameCocktail = Console.ReadLine();
+                if (nameCocktail == "Party!")
+                {
+                    Console.WriteLine($"We need {profit-allSum:F2} leva more.");
+                    break;
+                }
+                price = nameCocktail.Length;
+                 numCocktail = int.Parse(Console.ReadLine());
+                totalPrice = price * numCocktail;
+                
+                if (totalPrice%2!=0)
+                {
+                    totalPrice = totalPrice * 0.75;
+                }
+                allSum += totalPrice;
+            }
+            if (profit<=allSum)
+            {
+                Console.WriteLine("Target acquired.");
+            }
+            Console.WriteLine($"Club income - {allSum:F2} leva.");
+            
+            
+        }
+    }
+}
