a85b99881dc8198b67f6fa3551361c1bed34a131
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-18 14:39:00 +0300

    Added new problem from the exam
    Renovation

---
+++ CSharp-Programming-Basics/Exams/e3.Exam6-7July2019/04.Renovation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _04.Renovation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double high = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            double totalWall = Math.Ceiling((high * width*4) - ((high * width*4) * percent / 100));
+            string input = Console.ReadLine(); 
+            while ( input !="Tired!")
+            {
+                int paint = int.Parse(input);
+                totalWall -= paint;
+                if (totalWall<=0)
+                {
+                    break;
+                }
+                input =Console.ReadLine();
+            }
+            if (totalWall==0)
+            {
+                Console.WriteLine($"All walls are painted! Great job, Pesho!");
+            }
+            else if (totalWall<0)
+            {
+                Console.WriteLine($"All walls are painted and you have {Math.Abs(totalWall)} l paint left!");
+            }
+            else
+            {
+                Console.WriteLine($"{totalWall} quadratic m left.");
+            }
+        }
+    }
+}
