ae934c7e60e2cb26d4d52380f5572310bd271c0b
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-20 15:42:16 +0300

    Added new problem from the exam
    The most powerful word

---
+++ CSharp-Programming-Basics/Exams/e3.Exam6-7July2019/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "";
+            string mostPowerfulWord = "";
+            double maxPower = 0.0;
+            double totalSum = 0.0;
+
+            while (word != "End of words")
+            {
+
+                word = Console.ReadLine();
+
+                for (int i = 0; i < word.Length; i++)
+                {
+                    totalSum = word[i];
+                }
+                int numLength = word.Length;
+
+                string word2 = word.ToLower();
+                if ( word2[0] == 'a' || word2[0] == 'e' || word2[0] == 'i' || word2[0] == 'o' || word2[0] == 'u' || word2[0] == 'y')
+                {
+                    totalSum = totalSum * numLength;
+                }
+                else
+                {
+                    totalSum = Math.Floor(totalSum / numLength);
+                }
+                if (totalSum>maxPower)
+                {
+                    maxPower = totalSum;
+                    mostPowerfulWord = word;
+                }
+            }
+            Console.WriteLine($"The most powerful word is {mostPowerfulWord} - {maxPower}");
+        }
+    }
+}
