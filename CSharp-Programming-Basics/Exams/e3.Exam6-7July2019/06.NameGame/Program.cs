c6248fc6af9e7af2212f56f5e52a0d85ca536067
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-20 15:40:37 +0300

    Added new problem from the exam
    Name game

---
+++ CSharp-Programming-Basics/Exams/e3.Exam6-7July2019/06.NameGame/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _06.NameGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            int bestPoint = 0;
+            string winner = "";
+            string name = Console.ReadLine();
+
+            while (name!="Stop")
+            {
+               
+                int currentPoint = 0;
+                for (int i = 0; i < name.Length; i++)
+                {
+                    int number = int.Parse(Console.ReadLine());
+                    if (number == name[i])
+                    {
+                        currentPoint += 10;
+                    }
+                    else
+                    {
+                        currentPoint += 2;
+                    }                   
+                }
+                if (currentPoint >= bestPoint)
+                {
+                    bestPoint = currentPoint;
+                    winner = name;
+                }
+                name = Console.ReadLine();
+            }
+            Console.WriteLine($"The winner is {winner} with {bestPoint} points!");
+        }
+    }
+}
