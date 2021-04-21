d81800b19919dbfac4c85dce4197ba4a12440cd1
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-21 09:37:10 +0300

    Added new problem from the exam
    Tennis equipment

---
+++ CSharp-Programming-Basics/Exams/e4.Exam9-10Mart/01.TennisEquipment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01.TennisEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rocketPrice = double.Parse(Console.ReadLine());
+            int rocketQuantity = int.Parse(Console.ReadLine());
+            int sneakersQuantity = int.Parse(Console.ReadLine());
+
+            double sneakersPrice = rocketPrice / 6;
+            
+            double price = rocketPrice * rocketQuantity + sneakersPrice * sneakersQuantity;
+            double other = price * 0.20;
+            double totalPrice = price + other;
+            double djokovichPrice = totalPrice * 1 / 8;
+            double sponsorPrice = totalPrice * 7 / 8;
+            Console.WriteLine($"Price to be paid by Djokovic {Math.Floor(djokovichPrice)}");
+            Console.WriteLine($"Price to be paid by sponsors {Math.Ceiling(sponsorPrice)}");
+        }
+    }
+}
