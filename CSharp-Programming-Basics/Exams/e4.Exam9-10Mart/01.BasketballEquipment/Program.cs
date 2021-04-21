7ae8fed1acf08432750b8d3fdab3fd8bdff75e97
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-21 09:35:59 +0300

    Added new problem from the exam
    Basketball equipment

---
+++ CSharp-Programming-Basics/Exams/e4.Exam9-10Mart/01.BasketballEquipment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01.BasketballEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tax = double.Parse(Console.ReadLine());
+
+            double sneakers = tax * 0.60;
+            double outfit = sneakers * 0.80;
+            double ball = outfit / 4;
+            double other = ball / 5;
+            double total = tax + sneakers + outfit + ball + other;
+            Console.WriteLine($"{total:f2}");
+        }
+    }
+}
