e970bc36016bc2e04d67ada3f2367e53c6a0de27
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-22 16:39:08 +0300

    Added new problem from the exam
    Football results

---
+++ CSharp-Programming-Basics/Exams/e4.Exam9-10Mart/02.FootballResults/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _02.FootballResults
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int win = 0;
+            int loose = 0;
+            int drow = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                string mach = Console.ReadLine();
+                char score1 = mach[0];
+                char score2 = mach[2];
+                if (score1>score2)
+                {
+                    win++;
+                }
+                else if (score1 < score2)
+                {
+                    loose++;
+                }
+                else if (score1 == score2)
+                {
+                    drow++;
+                }
+            }
+            Console.WriteLine($"Team won {win} games.");
+            Console.WriteLine($"Team lost {loose} games.");
+            Console.WriteLine($"Drawn games: {drow}");
+        }
+    }
+}
