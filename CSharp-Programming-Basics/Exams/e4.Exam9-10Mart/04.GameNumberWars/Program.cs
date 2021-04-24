9ed3f913e4971936dbc8f8bb3dbfcbde26f87439
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-24 13:46:43 +0300

    Added new problem from the exam
    Game number wars

---
+++ CSharp-Programming-Basics/Exams/e4.Exam9-10Mart/04.GameNumberWars/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _04.GameNumberWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1 = Console.ReadLine();
+            string name2 = Console.ReadLine();
+
+            int numberWarpoint1 = 0;
+            int numberWarPoint2 = 0;
+            int totalPoint1 = 0;
+            int totalPoint2 = 0;
+            
+            string input = "";
+            string winnerNumberWars = "";
+
+            while ((input = Console.ReadLine()) != "End of game")
+            {
+                int pointName1 = 0;
+                int pointName2 = 0;
+                pointName1 = int.Parse(input);
+                pointName2 = int.Parse(Console.ReadLine());
+                if (pointName1>pointName2)
+                {
+                    totalPoint1 += pointName1 - pointName2;
+                }
+                else if (pointName2>pointName1)
+                {
+                    totalPoint2 += pointName2 - pointName1;
+                }
+                else if (pointName1==pointName2)
+                {
+                   numberWarpoint1 =int.Parse(Console.ReadLine());
+                   numberWarPoint2 =int.Parse(Console.ReadLine());
+                    if (numberWarpoint1>numberWarPoint2)
+                    {
+                        winnerNumberWars = name1;
+                        Console.WriteLine("Number wars!");
+                        Console.WriteLine($"{winnerNumberWars} is winner with {totalPoint1} points");
+                        break;
+                    }
+                    else if (numberWarPoint2>numberWarpoint1)
+                    {
+                        winnerNumberWars = name2;
+                        Console.WriteLine("Number wars!");
+                        Console.WriteLine($"{winnerNumberWars} is winner with {totalPoint2} points");
+                        break;
+                    }
+                }
+            }
+            if (input =="End of game")
+            {
+                Console.WriteLine($"{name1} has {totalPoint1} points");
+                Console.WriteLine($"{name2} has {totalPoint2} points");
+            }
+        }
+    }
+}
