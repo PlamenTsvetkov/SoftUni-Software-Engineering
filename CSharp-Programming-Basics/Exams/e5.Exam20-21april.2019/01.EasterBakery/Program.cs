dcf3e943a8ea689b32167320ddd43b87edc6e265
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-27 09:57:18 +0300

    Added new problem from the exam
    Easter bakery

---
+++ CSharp-Programming-Basics/Exams/e5.Exam20-21april.2019/01.EasterBakery/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01.EasterBakery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double branoPrice = double.Parse(Console.ReadLine());
+            double branoKg= double.Parse(Console.ReadLine());
+            double sugarKg = double.Parse(Console.ReadLine());
+            double quantityEggs = double.Parse(Console.ReadLine());
+            double mayaPacket = double.Parse(Console.ReadLine());
+            double sugarPrice = branoPrice * 0.75;
+            double eggsPrice = branoPrice * 1.1;
+            double mayaPrice = sugarPrice * 0.20;
+            double totalPrice = branoKg * branoPrice + sugarKg * sugarPrice + quantityEggs * eggsPrice + mayaPacket * mayaPrice;
+            Console.WriteLine($"{totalPrice:f2}");
+
+        }
+    }
+}
