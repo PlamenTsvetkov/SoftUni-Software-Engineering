ce7a5c21497e1f9e60bb4d5a5b3a87615bb7617f
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-27 09:58:43 +0300

    Added new problem from the exam
    Easter lunch

---
+++ CSharp-Programming-Basics/Exams/e5.Exam20-21april.2019/01.EasterLunch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01.EasterLunch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double quantityEasterBread = double.Parse(Console.ReadLine());
+            double quantityEggs = double.Parse(Console.ReadLine());
+            double qurabiiKg = double.Parse(Console.ReadLine());
+
+            double easterBreadPrice = 3.20;
+            double eggsPrice = 4.35;
+            double qurabiiPrice = 5.40;
+            double eggPaintPrice = 0.15;
+
+            double totalEasterBread = quantityEasterBread * easterBreadPrice;
+            double totalEggs = quantityEggs * eggsPrice;
+            double totalQuarabii = qurabiiKg * qurabiiPrice;
+            double paintEggs = quantityEggs * eggPaintPrice*12;
+            double totalExpenses = totalEasterBread + totalEggs + totalQuarabii + paintEggs;
+            Console.WriteLine($"{totalExpenses:f2}");
+           
+
+        }
+    }
+}
