48cd3add48b390b9c289b058f55ad4fd131a2d1a
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-04-28 10:34:11 +0300

    Added new problem from the exam
    Easter party

---
+++ CSharp-Programming-Basics/Exams/e5.Exam20-21april.2019/02.EasterParty/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _02.EasterParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityGuests = int.Parse(Console.ReadLine());
+            double couvertPrice = double.Parse(Console.ReadLine());
+            double budget = double.Parse(Console.ReadLine());
+            double couvertTotalPrice = 0;
+            if (quantityGuests >=10 && quantityGuests<=15)
+            {
+                couvertTotalPrice = quantityGuests * couvertPrice * 0.85;
+            }
+            else if (quantityGuests > 15 && quantityGuests <= 20)
+                {
+                couvertTotalPrice = quantityGuests * couvertPrice * 0.80;
+                }
+            else if (quantityGuests > 20)
+            {
+                couvertTotalPrice = quantityGuests * couvertPrice * 0.75;
+            }
+            if (quantityGuests < 10)
+            {
+                couvertTotalPrice = quantityGuests * couvertPrice;
+            }
+            double cakePrice = budget * 0.10;
+            double totalExpenses = cakePrice + couvertTotalPrice;
+            if (budget>=totalExpenses)
+            {
+                Console.WriteLine($"It is party time! {budget-totalExpenses:f2} leva left.");
+            }
+            else if (budget<totalExpenses)
+            {
+                Console.WriteLine($"No party! {totalExpenses-budget:f2} leva needed.");
+            }
+            
+        }
+    }
+}
