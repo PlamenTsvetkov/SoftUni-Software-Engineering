1eafb103c5f5af2c9cf903de0c1d8de3d1cba006
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-01 14:10:26 +0300

    Added a new task
    Easter eggs

---
+++ CSharp-Programming-Basics/Exams/e5.Exam20-21april.2019/05.EasterEggs/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _05.EasterEggs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double eggsNum = double.Parse(Console.ReadLine());
+            int redColor = 0;
+            int orangeColor = 0;
+            int blueColor = 0;
+            int greenColor = 0;
+            
+            for (int i = 0; i < eggsNum; i++)
+            {
+                string eggColor = Console.ReadLine();
+                if (eggColor=="red")
+                {
+                    redColor += 1;
+                }
+                else if (eggColor == "orange")
+                {
+                    orangeColor += 1;
+                }
+                else if (eggColor == "blue")
+                {
+                    blueColor += 1;
+                }
+                else if (eggColor == "green")
+                {
+                    greenColor += 1;
+                }
+            }
+            int maxEggsCount = redColor;
+            string maxEggsColor = "red";
+            if (maxEggsCount < orangeColor)
+            {
+                maxEggsCount = orangeColor;
+                maxEggsColor = "orange";
+            }
+            if (maxEggsCount < blueColor)
+            {
+                maxEggsCount = blueColor;
+                maxEggsColor = "blue";
+            }
+            if (maxEggsCount < greenColor)
+            {
+                maxEggsCount = greenColor;
+                maxEggsColor = "green";
+            }
+            Console.WriteLine($"Red eggs: {redColor}");
+            Console.WriteLine($"Orange eggs: {orangeColor}");
+            Console.WriteLine($"Blue eggs: {blueColor}");
+            Console.WriteLine($"Green eggs: {greenColor}");
+            Console.WriteLine($"Max eggs: {maxEggsCount} -> {maxEggsColor}");
+        }
+    }
+}
