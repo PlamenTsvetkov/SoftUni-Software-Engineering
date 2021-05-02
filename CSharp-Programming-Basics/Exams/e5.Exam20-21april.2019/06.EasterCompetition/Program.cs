aab9375245f75a27b48bc3a1303b78e196e225e5
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-02 09:33:25 +0300

    Added a new task
    Easter competition

---
+++ CSharp-Programming-Basics/Exams/e5.Exam20-21april.2019/06.EasterCompetition/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06.EasterCompetition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int easterBredNum = int.Parse(Console.ReadLine());
+
+           
+            int grade = 0;
+            int maxGrade = 0;            
+            string nameBaker = "";
+            string nameNumberOne = "";
+            string input = "";
+            for (int i = 0; i < easterBredNum; i++)
+            {
+                int sumGrade = 0;
+                nameBaker =Console.ReadLine();
+                while ((input=Console.ReadLine())!="Stop")
+                {
+                    grade = int.Parse(input);
+                    sumGrade += grade;
+                }
+                Console.WriteLine($"{nameBaker} has {sumGrade} points.");
+                if (maxGrade<sumGrade)
+                {
+                    maxGrade = sumGrade;
+                    nameNumberOne = nameBaker;
+                    Console.WriteLine($"{nameNumberOne} is the new number 1!");
+                }
+            }
+            Console.WriteLine($"{nameNumberOne} won competition with {maxGrade} points!");
+        }
+    }
+}
