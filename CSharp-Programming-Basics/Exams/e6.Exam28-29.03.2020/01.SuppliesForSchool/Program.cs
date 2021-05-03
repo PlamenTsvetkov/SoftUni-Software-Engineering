cb89aca94cc94fe0f9686c3eaaeb5634ceec48da
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-03 14:51:01 +0300

    Added a new task
    Supplies for school

---
+++ CSharp-Programming-Basics/Exams/e6.Exam28-29.03.2020/01.SuppliesForSchool/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _01.SuppliesForSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double quantityPensil = double.Parse(Console.ReadLine());
+            double quantityMarkers = double.Parse(Console.ReadLine());
+            double literPreparat = double.Parse(Console.ReadLine());
+            double percentDiscount = double.Parse(Console.ReadLine());
+
+            double pricePensil = 5.80;
+            double priceMarkers = 7.20;
+            double pricePreparat = 1.20;
+
+            double totalPricePensil = quantityPensil * pricePensil;
+            double totalPriceMarkers = quantityMarkers*priceMarkers;
+            double totalPricePreparat = literPreparat*pricePreparat;
+
+            double totalPrice = totalPricePensil + totalPriceMarkers + totalPricePreparat;
+
+            double totalPriceDiscount = totalPrice - (totalPrice * percentDiscount / 100);
+            Console.WriteLine($"{totalPriceDiscount:F3}");
+
+        }
+    }
+}
