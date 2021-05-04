762ee63cfb373323c6fd775b06eb0075535913ae
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-04 15:10:07 +0300

    Added a new task
    Cat walking

---
+++ CSharp-Programming-Basics/Exams/e6.Exam28-29.03.2020/02.CatWalking/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02.CatWalking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double minuteWalk = double.Parse(Console.ReadLine());
+            int numWalk = int.Parse(Console.ReadLine());
+            double catCalories = double.Parse(Console.ReadLine());
+
+            double totalMinute = minuteWalk * numWalk;
+            double burnCalories = totalMinute * 5;
+            double percenet = catCalories * 0.5;
+
+            if (percenet <= burnCalories)
+            {
+                Console.WriteLine($"Yes, the walk for your cat is enough. Burned calories per day: {burnCalories}.");
+            }
+            else
+            {
+                Console.WriteLine($"No, the walk for your cat is not enough. Burned calories per day: {burnCalories}.");
+            }
+        }
+    }
+}
