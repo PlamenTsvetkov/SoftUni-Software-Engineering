0c04495a30eda75dcf22a8b2684e0f2213bb91e0
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-04 15:13:20 +0300

    Added a new task
    Mountain run

---
+++ CSharp-Programming-Basics/Exams/e6.Exam28-29.03.2020/02.MountainRun/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _02.MountainRun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double secondRecords = double.Parse(Console.ReadLine());
+            double crossMetre = double.Parse(Console.ReadLine());
+            double timeForMetre = double.Parse(Console.ReadLine());
+
+            double otherSeconds = 0;
+            double otherFlood = Math.Floor(crossMetre / 50);
+            otherSeconds = otherFlood * 30;
+            double seconds = crossMetre * timeForMetre;
+            double totalSeconds = seconds + otherSeconds;
+            if (totalSeconds<secondRecords)
+            {
+                Console.WriteLine($"Yes! The new record is {totalSeconds:f2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No! He was {Math.Abs(secondRecords-totalSeconds):f2} seconds slower.");
+            }
+        }
+    }
+}
