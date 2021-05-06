0e1a8766bc97d8efc3c1d759f2a22b05008b1aad
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-06 14:30:49 +0300

    Added a new task
    Food for pet

---
+++ CSharp-Programming-Basics/Exams/e6.Exam28-29.03.2020/04.FoodForPets/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _04.FoodForPets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine());
+            double totalFood = double.Parse(Console.ReadLine());
+            double currentBiscuit = 0;
+            double totalBiscuit = 0;
+            double eatFood = 0;
+            double totalEatCat=0;
+            double totalEatDog = 0;
+
+            for (int i = 1; i <= day; i++)
+            {
+                double eatDog = double.Parse(Console.ReadLine());
+                eatFood += eatDog;
+                totalEatDog += eatDog;
+              double eatCat = double.Parse(Console.ReadLine());
+                eatFood += eatCat;
+                totalEatCat += eatCat;
+                if (i%3==0)
+                {
+                   currentBiscuit = (eatCat + eatDog) * 0.1;
+                    totalBiscuit += currentBiscuit;
+                }
+            }
+            Console.WriteLine($"Total eaten biscuits: {Math.Round(totalBiscuit)}gr.");
+            Console.WriteLine($"{eatFood/totalFood*100:f2}% of the food has been eaten.");
+            Console.WriteLine($"{totalEatDog / eatFood * 100:f2}% eaten from the dog.");
+            Console.WriteLine($"{totalEatCat / eatFood * 100:f2}% eaten from the cat.");
+        }
+    }
+}
