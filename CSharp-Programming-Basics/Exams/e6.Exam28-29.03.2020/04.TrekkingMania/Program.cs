c5a01ab2a19c055ba0689fe968ca067791f6df00
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-06 14:37:04 +0300

    Added a new task
    Trekking mania

---
+++ CSharp-Programming-Basics/Exams/e6.Exam28-29.03.2020/04.TrekkingMania/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _04.TrekkingMania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int group = int.Parse(Console.ReadLine());
+            double totalPerson = 0;
+            double totalMusala = 0;
+            double totalMonblan = 0;
+            double totalKilimandjaro = 0;
+            double totalK2 = 0;
+            double totalEverest = 0;
+            for (int i = 0; i < group; i++)
+            {
+                int groupNum = int.Parse(Console.ReadLine());
+                totalPerson += groupNum;
+                if (groupNum<=5)
+                {
+                    totalMusala += groupNum;
+                }
+                else if (5<groupNum && groupNum<=12)
+                {
+                    totalMonblan += groupNum;
+                }
+                else if (12 < groupNum && groupNum <= 25)
+                {
+                    totalKilimandjaro += groupNum;
+                }
+                else if (25 < groupNum && groupNum <= 40)
+                {
+                    totalK2 += groupNum;
+                }
+                else if (40 < groupNum)
+                {
+                    totalEverest += groupNum;
+                }
+            }
+            Console.WriteLine($"{totalMusala/totalPerson*100:f2}%");
+            Console.WriteLine($"{totalMonblan / totalPerson * 100:f2}%");
+            Console.WriteLine($"{totalKilimandjaro / totalPerson * 100:f2}%");
+            Console.WriteLine($"{totalK2 / totalPerson * 100:f2}%");
+            Console.WriteLine($"{totalEverest / totalPerson * 100:f2}%");
+
+
+        }
+    }
+}
