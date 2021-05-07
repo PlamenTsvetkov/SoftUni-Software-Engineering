45531794a166c0cc942cf44fb6282856da3a8830
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-07 09:52:40 +0300

    Added a new task
    Care of puppy

---
+++ CSharp-Programming-Basics/Exams/e6.Exam28-29.03.2020/05.CareOfPuppy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05.CareOfPuppy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double kgFood = double.Parse(Console.ReadLine());
+            string isAdopted = "";
+            double totalGrFood = kgFood * 1000;
+            double totalEat = 0;
+            while ((isAdopted=Console.ReadLine())!="Adopted")
+            {
+                int grFood = int.Parse(isAdopted);
+                totalEat += grFood;
+            }
+            if (totalGrFood>=totalEat)
+            {
+                Console.WriteLine($"Food is enough! Leftovers: {totalGrFood-totalEat} grams.");
+            }
+            else
+            {
+                Console.WriteLine($"Food is not enough. You need {totalEat-totalGrFood} grams more.");
+            }
+        }
+    }
+}
