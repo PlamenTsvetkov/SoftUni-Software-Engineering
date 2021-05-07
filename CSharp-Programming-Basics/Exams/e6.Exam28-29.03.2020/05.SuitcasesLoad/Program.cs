34d0dbe62e001956869d23bfd45bc0f90c5723c8
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-07 09:53:37 +0300

    Added a new task
    Suitcase load

---
+++ CSharp-Programming-Basics/Exams/e6.Exam28-29.03.2020/05.SuitcasesLoad/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _05.SuitcasesLoad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double capacity = double.Parse(Console.ReadLine());
+            string isEnd = "";
+            int is3 = 0;
+            double totalObem = 0;
+            int suitcase = 0;
+            while ((isEnd = Console.ReadLine())!="End")
+            {
+                double obem = double.Parse(isEnd);
+                is3 += 1;
+                if (is3%3==0)
+                {
+                    obem = obem * 1.1;
+                }
+                if (capacity-totalObem<obem)
+                {
+                    Console.WriteLine($"No more space!");
+                    break;
+                }
+                suitcase += 1;
+                totalObem += obem;
+            }
+            if (isEnd=="End")
+            {
+                Console.WriteLine($"Congratulations! All suitcases are loaded!");
+            }
+            Console.WriteLine($"Statistic: {suitcase} suitcases loaded.");
+        }
+    }
+}
