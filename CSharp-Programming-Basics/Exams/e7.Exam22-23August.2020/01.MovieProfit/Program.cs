8730b8bd1c7933aed80029ce36cfca1f8217e851
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-08 10:16:41 +0300

    Added a new task
    Movie profit

---
+++ CSharp-Programming-Basics/Exams/e7.Exam22-23August.2020/01.MovieProfit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01.MovieProfit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameFilm = Console.ReadLine();
+            int days = int.Parse(Console.ReadLine());
+            int ticketNum = int.Parse(Console.ReadLine());
+            double ticketPrice = double.Parse(Console.ReadLine());
+            int percent = int.Parse(Console.ReadLine());
+
+            double totalDaySum = ticketNum * ticketPrice;
+            double totalSumPeriod = totalDaySum * days;
+            double cinemaExpenses = totalSumPeriod * percent / 100;
+            double profit = totalSumPeriod - cinemaExpenses;
+            Console.WriteLine($"The profit from the movie {nameFilm} is {profit:f2} lv.");
+        }
+    }
+}
