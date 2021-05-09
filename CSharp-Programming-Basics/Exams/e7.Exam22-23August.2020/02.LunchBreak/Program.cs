fe5b14446d4104654b87d17a5f87c0704d525909
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-09 14:12:56 +0300

    Added a new task
    Lunch break

---
+++ CSharp-Programming-Basics/Exams/e7.Exam22-23August.2020/02.LunchBreak/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02.LunchBreak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameSerial = Console.ReadLine();
+            double timeEpizod = double.Parse(Console.ReadLine());
+            double stop = double.Parse(Console.ReadLine());
+
+            double timeLunch = stop * 1 / 8;
+            double timeRelax = stop * 1 / 4;
+            double freeTime = stop - timeLunch - timeRelax;
+
+            if (freeTime>=timeEpizod)
+            {
+                Console.WriteLine($"You have enough time to watch {nameSerial} and left with {Math.Ceiling(freeTime-timeEpizod)} minutes free time.");
+            }
+            else
+            {
+                Console.WriteLine($"You don't have enough time to watch {nameSerial}, you need {Math.Ceiling(timeEpizod-freeTime)} more minutes.");
+            }
+        }
+    }
+}
