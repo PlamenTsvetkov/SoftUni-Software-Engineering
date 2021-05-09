7ca76e5e28ef274beb606552a5cfa3761b28e2d0
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-09 14:11:35 +0300

    Added a new task
    Series calculator

---
+++ CSharp-Programming-Basics/Exams/e7.Exam22-23August.2020/01.SeriesCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01.SeriesCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameSerial = Console.ReadLine();
+            double seasonNum = double.Parse(Console.ReadLine());
+            double episodsNum = double.Parse(Console.ReadLine());
+            double timeEpisod = double.Parse(Console.ReadLine());
+
+            double advertisingTime = timeEpisod * 0.2;
+            double specialEpizodTime = 10 * seasonNum;
+            double timeEpizodAdvertising = advertisingTime + timeEpisod;
+            double totalTimeAdvertising = timeEpizodAdvertising * episodsNum * seasonNum + specialEpizodTime;
+            Console.WriteLine($"Total time needed to watch the {nameSerial} series is {Math.Floor(totalTimeAdvertising)} minutes.");
+        }
+    }
+}
