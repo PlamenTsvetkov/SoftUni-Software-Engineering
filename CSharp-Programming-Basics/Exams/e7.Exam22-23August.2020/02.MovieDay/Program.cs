f02b2e4adfd9688025169d6fd0518d5669beffcf
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-10 10:55:44 +0300

    Added a new task
    Movie day

---
+++ CSharp-Programming-Basics/Exams/e7.Exam22-23August.2020/02.MovieDay/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _02.MovieDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double time = double.Parse(Console.ReadLine());
+            double sceneNum = double.Parse(Console.ReadLine());
+            double sceneTime = double.Parse(Console.ReadLine());
+
+            double prepare = time * 0.15;
+            double totalScenaTime = sceneNum * sceneTime;
+            double totalTime = prepare + totalScenaTime;
+
+            if (totalTime<= time)
+            {
+                Console.WriteLine($"You managed to finish the movie on time! You have {Math.Round(time-totalTime,0)} minutes left!");
+            }
+            else
+            {
+                Console.WriteLine($"Time is up! To complete the movie you need {totalTime-time} minutes.");
+            }
+
+
+        }
+    }
+}
