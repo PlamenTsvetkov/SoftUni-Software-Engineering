1e887c30cf2d77d5f3bf46f1275c70ab10e83391
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-12 10:08:31 +0300

    Added a new task
    Oscars

---
+++ CSharp-Programming-Basics/Exams/e7.Exam22-23August.2020/05.Oscars/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _05.Oscars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameActior = Console.ReadLine();
+         double point = double.Parse(Console.ReadLine());
+         double pointer = double.Parse(Console.ReadLine());
+            for (int i = 0; i < pointer; i++)
+            {
+                double currentPoint = 0;
+                string namePointer = Console.ReadLine();
+               double pointPointer = double.Parse(Console.ReadLine());
+                currentPoint = namePointer.Length * pointPointer / 2;
+                point += currentPoint;
+                if (point>1250.50)
+                {
+                    Console.WriteLine($"Congratulations, {nameActior} got a nominee for leading role with {point:f1}!");
+                    break;
+                }
+                
+            }
+            if (point<1250.50)
+            {
+                Console.WriteLine($"Sorry, {nameActior} you need {1250.50-point:f1} more!");
+            }
+        }
+    }
+}
