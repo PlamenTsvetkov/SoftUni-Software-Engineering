735ab288299633365d51ca247f26c2ce6592af38
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-13 10:15:09 +0300

    Added a new task
    Favorite movie

---
+++ CSharp-Programming-Basics/Exams/e7.Exam22-23August.2020/06.FavouriteMovie/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _06.FavouriteMovie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string movie = "";
+            string theBestMovie = "";
+            int counter = 0;
+            int maxSymvol = int.MinValue;
+            while ((movie = Console.ReadLine())!="STOP")
+            {
+                counter++;
+                if (counter>=7)
+                {
+                    Console.WriteLine("The limit is reached.");
+                    break;
+                }
+                int sumSymvol = 0;
+                for (int j = 0; j < movie.Length; j++)
+                {
+                    if (Char.IsUpper(movie[j]))
+                    {
+                        sumSymvol += movie[j] - movie.Length;
+                    }
+                    else if (Char.IsLower(movie[j]))
+                    {
+                        sumSymvol += movie[j] -(2* movie.Length);
+                    }
+                    else
+                    {
+                        sumSymvol += movie[j];
+                    }
+                }
+                if (sumSymvol > maxSymvol)
+                {
+                    maxSymvol = sumSymvol;
+                    theBestMovie = movie;
+                }
+                
+            }
+            Console.WriteLine($"The best movie for you is {theBestMovie} with {maxSymvol} ASCII sum.");
+
+        }
+    }
+}
