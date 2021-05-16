903431259b133f3bd06e27c6a695b5923073fcf5
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-16 14:09:41 +0300

    Added a new task
    Calorie calculator

---
+++ CSharp-Programming-Basics/Exams/e8.Exam12.12.2020/02/calorieCalculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _02
+{
+    class calorieCalculator
+    {
+        static void Main(string[] args)
+        {
+            char gender = char.Parse(Console.ReadLine());
+            double kg = double.Parse(Console.ReadLine());
+            double heightM = double.Parse(Console.ReadLine());
+            double age = double.Parse(Console.ReadLine());
+            string activity =Console.ReadLine();
+
+            double bhm = 0;
+            double calories = 0;
+
+            if (gender=='m')
+            {
+                bhm = 66 + (13.7 * kg) + (5 * heightM * 100) - (6.8 * age);
+            }
+            else
+            {
+                bhm = 655 + (9.6 * kg) + (1.8 * heightM * 100) - (4.7 * age);
+            }
+
+            if (activity=="sedentary")
+            {
+                calories = bhm * 1.2;
+            }
+            else if (activity == "lightly active")
+            {
+                calories = bhm * 1.375;
+            }
+            else if (activity == "moderately active")
+            {
+                calories = bhm * 1.55;
+            }
+            else if (activity == "very active")
+            {
+                calories = bhm * 1.725;
+            }
+
+            Console.WriteLine($"To maintain your current weight you will need {Math.Ceiling(calories)} calories per day.");
+        }
+    }
+}
