844a2f6b91413bfee380e84fbadc41679b1a2d00
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-18 18:14:17 +0300

    Added a new task
    Gold mine

---
+++ CSharp-Programming-Basics/Exams/e8.Exam12.12.2020/06/goldMine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _06
+{
+    class goldMine
+    {
+        static void Main(string[] args)
+        {
+            int locationNumber = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < locationNumber; i++)
+            {
+                double totalGoldMined = 0;
+                double averageTargetPerDay = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                for (int j = 0; j < days; j++)
+                {
+                    double goldMinedPerDay = double.Parse(Console.ReadLine());
+                    totalGoldMined += goldMinedPerDay;
+                }
+                double averageGoldPerDay = totalGoldMined / days;
+                if (averageGoldPerDay>=averageTargetPerDay)
+                {
+                    Console.WriteLine($"Good job! Average gold per day: {averageGoldPerDay:f2}.");
+                }
+                else
+                {
+                    Console.WriteLine($"You need {averageTargetPerDay-averageGoldPerDay:f2} gold.");
+                }
+            }
+        }
+    }
+}
