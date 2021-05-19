5185e82c98004a74b1b4dc1cdc611079fb4db981
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-19 18:04:16 +0300

    Added a new task
    Passed

---
+++ CSharp-Programming-Fundamentals/01.1.basicsSyntaxConditionalStatementsandLoops/02Passed/Passed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _02Passed
+{
+    class Passed
+    {
+        static void Main(string[] args)
+        {
+            double averageGrade = double.Parse(Console.ReadLine());
+
+            if (averageGrade>=3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+        }
+    }
+}
