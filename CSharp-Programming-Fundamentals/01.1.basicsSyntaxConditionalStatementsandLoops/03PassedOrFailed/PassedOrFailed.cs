55fc5ab9000fe1346199c098b0421500df935f88
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-19 18:05:48 +0300

    Added a new task
    Passed or failed

---
+++ CSharp-Programming-Fundamentals/01.1.basicsSyntaxConditionalStatementsandLoops/03PassedOrFailed/PassedOrFailed.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03PassedOrFailed
+{
+    class PassedOrFailed
+    {
+        static void Main(string[] args)
+        {
+            double averageGrade = double.Parse(Console.ReadLine());
+
+            if (averageGrade >= 3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+            else
+            {
+                Console.WriteLine("Failed!");
+            }
+        }
+    }
+}
