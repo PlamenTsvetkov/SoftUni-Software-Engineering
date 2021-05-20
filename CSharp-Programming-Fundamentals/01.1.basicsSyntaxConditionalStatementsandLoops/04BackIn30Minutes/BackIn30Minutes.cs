15b6a7e047d42bd7ad0cfaf694072c29cb1906e0
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-20 10:50:44 +0300

    Added a new task
    Back in 30 minutes

---
+++ CSharp-Programming-Fundamentals/01.1.basicsSyntaxConditionalStatementsandLoops/04BackIn30Minutes/BackIn30Minutes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _04BackIn30Minutes
+{
+    class BackIn30Minutes
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int plusMinutes = 30;
+
+            if (minutes+plusMinutes>59)
+            {
+                if (hour==23)
+                {
+                    hour = 0;
+                    minutes = minutes + 30-60;
+                }
+                else
+                {
+                    hour += 1;
+                    minutes = minutes + 30 - 60;
+                }
+            }
+            else if (minutes + plusMinutes <= 59)
+            {
+                minutes += 30;
+            }
+            Console.WriteLine($"{hour}:{minutes:d2}");
+        }
+    }
+}
