75f1c5010fe95814e8083a3521549542ce0fff64
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-21 11:44:40 +0300

    Added a new task
    Foreign languages

---
+++ CSharp-Programming-Fundamentals/01.1.basicsSyntaxConditionalStatementsandLoops/06foreignLanguages/foreignLanguages.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _06foreignLanguages
+{
+    class foreignLanguages
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+            string language = "";
+            if (country == "England" || country == "USA")
+            {
+                language = "English";
+            }
+            else if (country == "Spain" || country == "Argentina" || country == "Mexico")
+            {
+                language = "Spanish";
+            }
+            else
+            {
+                language = "unknown";
+            }
+            Console.WriteLine(language);
+        }
+    }
+}
