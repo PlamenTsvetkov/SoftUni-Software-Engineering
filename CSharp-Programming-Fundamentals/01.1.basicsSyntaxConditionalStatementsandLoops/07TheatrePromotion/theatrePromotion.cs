ef022dc057725115edbfd148203f4d097fa9de00
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-22 13:14:46 +0300

    Added a new task
    Theatre promotion

---
+++ CSharp-Programming-Fundamentals/01.1.basicsSyntaxConditionalStatementsandLoops/07TheatrePromotion/theatrePromotion.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace _07TheatrePromotion
+{
+    class theatrePromotion
+    {
+        static void Main(string[] args)
+        {
+            string day = Console.ReadLine().ToLower();
+            int age = int.Parse(Console.ReadLine());
+            int price = 0;
+
+            if (day=="weekday")
+            {
+                if (age >=0 && age <=18)
+                {
+                    price = 12;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    price = 18;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    price = 12;
+                }
+            }
+            else if (day == "weekend")
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    price = 15;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    price = 20;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    price = 15;
+                }
+            }
+            else if (day == "holiday")
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    price = 5;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    price = 12;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    price = 10;
+                }
+            }
+            if (age <0 || age >122)
+            {
+                Console.WriteLine("Error!");
+            }
+            else
+            {
+                Console.WriteLine($"{price}$");
+            }
+        }
+    }
+}
