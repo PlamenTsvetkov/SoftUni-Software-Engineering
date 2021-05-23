1464e8d131cff6c3fb7e1c212b8299462520d1f9
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-23 13:18:39 +0300

    Added a new task
    Sum of Odd numbers

---
+++ CSharp-Programming-Fundamentals/01.1.basicsSyntaxConditionalStatementsandLoops/09SumOfOddNumbers/sumOfOddNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _09SumOfOddNumbers
+{
+    class sumOfOddNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int totalNum = 0;
+            for (int i = 1; i < 2*n; i+=2)
+            {
+                Console.WriteLine(i);
+                totalNum += i;
+            }
+            Console.WriteLine($"Sum: {totalNum}");
+        }
+    }
+}
