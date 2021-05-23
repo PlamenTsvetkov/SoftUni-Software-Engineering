22ff70eabfbf0b242790e53f6a1c61788258fb12
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-24 00:14:20 +0300

    Added a new task
    Sum of odd numbers with while

---
+++ CSharp-Programming-Fundamentals/01.1.basicsSyntaxConditionalStatementsandLoops/09SumOfOddNumbersWhile/sumOfOddNumbersWhile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _09SumOfOddNumbersWhile
+{
+    class sumOfOddNumbersWhile
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int totalN = 0;
+            int totalNum = 0;
+            int num = 1;
+            while (totalN!=n)
+            {
+                totalNum += num;
+                Console.WriteLine(num);
+                num += 2;
+                totalN += 1;
+            }
+            Console.WriteLine($"Sum: {totalNum}");
+        }
+    }
+}
