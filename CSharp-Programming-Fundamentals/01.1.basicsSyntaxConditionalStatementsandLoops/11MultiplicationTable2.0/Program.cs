4476d1bf3e273cf1c6eb0c9e7132f4bda5bfcc5a
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-25 09:50:35 +0300

    Added a nwe task
    Multiplication table 2.0

---
+++ CSharp-Programming-Fundamentals/01.1.basicsSyntaxConditionalStatementsandLoops/11MultiplicationTable2.0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _11MultiplicationTable2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int times = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine($"{n} X {times} = {n * times}");
+                times += 1;
+            }
+            while (times < 11);
+        }
+    }
+}
