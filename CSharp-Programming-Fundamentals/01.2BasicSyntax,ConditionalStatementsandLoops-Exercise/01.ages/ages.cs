dc8d13973e7e42d6163d652744d0d8c18b46b29b
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-27 17:42:24 +0300

    Added a new task
    Ages

---
+++ CSharp-Programming-Fundamentals/01.2BasicSyntax,ConditionalStatementsandLoops-Exercise/01.ages/ages.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01.ages
+{
+    class ages
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num <=2)
+            {
+                Console.WriteLine("baby");
+            }
+            else if (3<=num && num <= 13)
+            {
+                Console.WriteLine("child");
+            }
+            else if (14 <= num && num <= 19)
+            {
+                Console.WriteLine("teenager");
+            }
+            else if (20 <= num && num <= 65)
+            {
+                Console.WriteLine("adult");
+            }
+            else if (66 <= num)
+            {
+                Console.WriteLine("elder");
+            }
+        }
+    }
+}
