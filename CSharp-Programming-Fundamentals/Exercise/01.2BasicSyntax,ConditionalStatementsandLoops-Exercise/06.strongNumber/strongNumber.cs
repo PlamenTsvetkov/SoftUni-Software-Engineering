b371a2cd7f542e48a1f369ba2c9fc8a19221c047
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-29 13:34:08 +0300

    Added a new task
    Strong number

---
+++ CSharp-Programming-Fundamentals/Exercise/01.2BasicSyntax,ConditionalStatementsandLoops-Exercise/06.strongNumber/strongNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _06.strongNumber
+{
+    class strongNumber
+    {
+        static void Main(string[] args)
+        {
+
+            int input = int.Parse(Console.ReadLine());
+            int number = input;
+            int currentNum = 0;
+            int totalSum = 0;
+            while (number!=0)
+            {
+                currentNum = number % 10;
+                int factorial = 1;
+                for (int i = 1; i <= currentNum; i++)
+                {
+                    factorial *= i;
+                }
+                totalSum += factorial;
+                number /= 10;
+            }
+            if (totalSum==input)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
