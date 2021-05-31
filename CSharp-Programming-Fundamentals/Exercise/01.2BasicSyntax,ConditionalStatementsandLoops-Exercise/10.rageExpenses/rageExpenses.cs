9bcc9e29f461e0f207ce2bec8452daa5bf5742d7
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-31 10:11:27 +0300

    Added a new task
    Rage expenses

---
+++ CSharp-Programming-Fundamentals/Exercise/01.2BasicSyntax,ConditionalStatementsandLoops-Exercise/10.rageExpenses/rageExpenses.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _10.rageExpenses
+{
+    class rageExpenses
+    {
+        static void Main(string[] args)
+        {
+            int lostGame = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            int headsetQuantity = lostGame / 2;
+            int mouseQuantity = lostGame / 3;
+            int keyboardQuantity = lostGame/6;
+            int displayQuantity=keyboardQuantity/2;
+           
+            double totalExpenses = headsetPrice * headsetQuantity + mousePrice * mouseQuantity + keyboardPrice * keyboardQuantity + displayPrice * displayQuantity;
+           Console.WriteLine($"Rage expenses: {totalExpenses:f2} lv.");
+        }
+    }
+}
