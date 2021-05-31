f4d4298a617d2a649c3d5974625e89193e477660
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-05-31 10:09:56 +0300

    Added a new task
    Padawan equipment

---
+++ CSharp-Programming-Fundamentals/Exercise/01.2BasicSyntax,ConditionalStatementsandLoops-Exercise/09.padawanEquipment/padawanEquipment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _09.padawanEquipment
+{
+    class padawanEquipment
+    {
+        static void Main(string[] args)
+        {
+            double amountMoney = double.Parse(Console.ReadLine());
+            int countStudents = int.Parse(Console.ReadLine());
+            double lightsabersPrice = double.Parse(Console.ReadLine());
+            double robesPrice = double.Parse(Console.ReadLine());
+            double beltsPrice = double.Parse(Console.ReadLine());
+
+            double lightsabresQuantity = Math.Ceiling(countStudents * 1.1);
+            double robesQuantity = countStudents;
+            double beltsQuantity = countStudents- (countStudents/6);
+
+            double totalExpenses = lightsabersPrice * lightsabresQuantity + robesPrice * robesQuantity + beltsPrice * beltsQuantity;
+            if (totalExpenses<=amountMoney)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalExpenses:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivan Cho will need {totalExpenses-amountMoney:f2}lv more.");
+            }
+        }
+    }
+}
