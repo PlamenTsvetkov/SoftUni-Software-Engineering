8e656304881d28d9fa29589f06a4425a995d1ee7
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-01 17:40:53 +0300

    Added a new task
    Pounds to dollars

---
+++ CSharp-Programming-Fundamentals/Labs/02.1dataTypesAndVariables-Lab/02.poundsToDollars/poundsToDollars.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02.poundsToDollars
+{
+    class poundsToDollars
+    {
+        static void Main(string[] args)
+        {
+            decimal pounds = decimal.Parse(Console.ReadLine());
+            decimal dollars = pounds * 1.31M;
+            Console.WriteLine($"{dollars:f3}");
+        }
+    }
+}
