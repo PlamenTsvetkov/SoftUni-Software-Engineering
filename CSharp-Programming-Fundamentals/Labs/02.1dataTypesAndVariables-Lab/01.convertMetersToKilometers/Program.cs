5fb8170265145e25f0181461a2bf78515fe77d83
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-01 17:39:45 +0300

    Added a new task
    Convert m to km

---
+++ CSharp-Programming-Fundamentals/Labs/02.1dataTypesAndVariables-Lab/01.convertMetersToKilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01.convertMetersToKilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int meters = int.Parse(Console.ReadLine());
+            decimal kilometres = meters / 1000.0M;
+            Console.WriteLine($"{kilometres:f2}");
+        }
+    }
+}
