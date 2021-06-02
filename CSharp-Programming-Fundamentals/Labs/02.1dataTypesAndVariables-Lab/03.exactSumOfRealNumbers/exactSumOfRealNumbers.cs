923ec83fb21a5c85d4481458c5208c9364d93b2d
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-02 17:04:54 +0300

    Added a new task
    Exact sum of real numbers

---
+++ CSharp-Programming-Fundamentals/Labs/02.1dataTypesAndVariables-Lab/03.exactSumOfRealNumbers/exactSumOfRealNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _03.exactSumOfRealNumbers
+{
+    class exactSumOfRealNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal sum = 0.0M;
+            for (int i = 0; i < n; i++)
+            {
+                decimal num = decimal.Parse(Console.ReadLine());
+                sum += num;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
