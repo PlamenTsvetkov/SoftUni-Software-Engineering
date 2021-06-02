d9a9801cb46bc6130cdaf8201b7a58d89de8cbc8
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-02 17:07:19 +0300

    Added a new task
    Centuries to minutes

---
+++ CSharp-Programming-Fundamentals/Labs/02.1dataTypesAndVariables-Lab/04.centuriesToMinutes/centuriesToMinutes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _04.centuriesToMinutes
+{
+    class centuriesToMinutes
+    {
+        static void Main(string[] args)
+        {
+            int centuries = int.Parse(Console.ReadLine());
+            int year = centuries * 100;
+            int days = (int)(year * 365.2422);
+            int hours = 24 * days;
+            int minutes = 60 * hours;
+            Console.WriteLine($"{centuries} centuries = {year} years = {days} days = {hours} hours = {minutes} minutes");
+        }
+    }
+}
