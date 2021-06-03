a215cfc6ac62ae399b32c4ef3c5b19d53d1bb94c
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-03 10:33:16 +0300

    Added a new task
    Special numbers

---
+++ CSharp-Programming-Fundamentals/Labs/02.1dataTypesAndVariables-Lab/05.specialNumbers/specialNumbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _05.specialNumbers
+{
+    class specialNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                int num = i;
+                int sum = 0;
+                while (num!=0)
+                {
+                    sum += num % 10;
+                    num = num / 10;
+                }
+                if (sum==5)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else if (sum==7)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else if (sum == 11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+            }
+        }
+    }
+}
