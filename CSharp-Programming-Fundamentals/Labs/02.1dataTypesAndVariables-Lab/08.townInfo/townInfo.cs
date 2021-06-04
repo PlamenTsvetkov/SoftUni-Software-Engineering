d6dc295dbc6d850e56aa8d1f7ef29a2a3cc55906
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-04 18:07:49 +0300

    Added a new task
    Town info

---
+++ CSharp-Programming-Fundamentals/Labs/02.1dataTypesAndVariables-Lab/08.townInfo/townInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _08.townInfo
+{
+    class townInfo
+    {
+        static void Main(string[] args)
+        {
+            string cityName = Console.ReadLine();
+            int population = int.Parse(Console.ReadLine());
+            int area = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Town {cityName} has population of {population} and area {area} square km.");
+        }
+    }
+}
