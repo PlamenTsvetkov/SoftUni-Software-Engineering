e76d97fbda55dd862fe3df68d0376102cfbd62e6
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-05 14:38:40 +0300

    Added a new task
    Lower or upper

---
+++ CSharp-Programming-Fundamentals/Labs/02.1dataTypesAndVariables-Lab/10.lowerOrUpper/lowerOrUpper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _10.lowerOrUpper
+{
+    class lowerOrUpper
+    {
+        static void Main(string[] args)
+        {
+            char char1 = char.Parse(Console.ReadLine());
+
+            if (char.IsLower(char1))
+            {
+                Console.WriteLine("lower-case");
+            }
+            else if (char.IsUpper(char1))
+            {
+                Console.WriteLine("upper-case");
+            }
+        }
+    }
+}
