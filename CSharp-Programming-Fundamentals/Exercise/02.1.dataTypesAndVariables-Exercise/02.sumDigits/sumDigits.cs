c8105c8b5a1dedad967322823dc50c7e24f28903
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-06 09:16:23 +0300

    Added a new task
    Sum digits

---
+++ CSharp-Programming-Fundamentals/Exercise/02.1.dataTypesAndVariables-Exercise/02.sumDigits/sumDigits.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _02.sumDigits
+{
+    class sumDigits
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int sum = 0;
+            while (num1!=0)
+            {
+                sum += num1 % 10;
+                num1 = num1 / 10;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
