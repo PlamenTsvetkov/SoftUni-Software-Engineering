0e9bdaa794fe0278b87eca5f4bd75a0ff79a861e
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-06 09:18:03 +0300

    Added a new task
    Sum digits

---
+++ CSharp-Programming-Fundamentals/Exercise/02.1.dataTypesAndVariables-Exercise/02.sumDigits2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02.sumDigits2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num1 = Console.ReadLine();
+            int sum = 0;
+            for (int i = 0; i < num1.Length; i++)
+            {
+                //int currentNum = (int)char.GetNumericValue(num1[i]); Вариант 1
+                int currentNum = int.Parse(num1[i].ToString());
+                sum += currentNum;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
