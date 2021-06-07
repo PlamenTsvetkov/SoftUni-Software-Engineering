6b67cfa237643e113b3a18360df4bf8a80455126
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-07 18:02:37 +0300

    Added a new task
    Elevator

---
+++ CSharp-Programming-Fundamentals/Exercise/02.1.dataTypesAndVariables-Exercise/03.elevator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03.elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte n = byte.Parse(Console.ReadLine());
+            byte p = byte.Parse(Console.ReadLine());
+            float courses = n / p ;
+            if (n%p!=0)
+            {
+                courses += 1;
+            }
+            Console.WriteLine(courses);
+
+
+        }
+    }
+}
