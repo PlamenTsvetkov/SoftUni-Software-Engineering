91d1934b6de5c1615f3b4caa8cbbc6e250b9a4c8
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-08 15:04:29 +0300

    Added a new task
    Sum of chars

---
+++ CSharp-Programming-Fundamentals/Exercise/02.1.dataTypesAndVariables-Exercise/04.sumOfChars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04.sumOfChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte n = byte.Parse(Console.ReadLine());
+            int intLetter = 0;
+            int sumLetter = 0;
+            for (int i = 0; i < n; i++)
+            {
+                char letter = char.Parse(Console.ReadLine());
+                intLetter = (int)letter;
+                sumLetter += intLetter;
+            }
+            Console.WriteLine($"The sum equals: {sumLetter}");
+        }
+    }
+}
