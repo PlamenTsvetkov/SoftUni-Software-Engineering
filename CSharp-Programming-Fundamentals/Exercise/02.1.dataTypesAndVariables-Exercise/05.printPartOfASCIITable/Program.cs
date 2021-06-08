336dbf064e1fe3eebc57347ec3dd5e709bc56efd
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-08 15:05:37 +0300

    Added a new task
    Print part of ASCII table

---
+++ CSharp-Programming-Fundamentals/Exercise/02.1.dataTypesAndVariables-Exercise/05.printPartOfASCIITable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _05.printPartOfASCIITable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte startNum = byte.Parse(Console.ReadLine());
+            byte finalNum = byte.Parse(Console.ReadLine());
+
+            for (int i = startNum; i <= finalNum; i++)
+            {
+                Console.Write($"{(char)i} ");
+            }
+
+        }
+    }
+}
