a4e31cd052a29111f641fb8d4d627e70d0011518
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-09 17:11:06 +0300

    Added a new task
    Water over flow

---
+++ CSharp-Programming-Fundamentals/Exercise/02.1.dataTypesAndVariables-Exercise/07.waterOverflow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _07.waterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int totalLiter = 0;
+            
+            for (int i = 0; i < n; i++)
+            {
+                int liter = int.Parse(Console.ReadLine());
+                byte capacity = 255;
+                if (totalLiter+liter>capacity)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    continue;
+                }
+                totalLiter += liter;
+            }
+            Console.WriteLine(totalLiter);
+        }
+    }
+}
