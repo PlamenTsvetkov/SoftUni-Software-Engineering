879928f2d9ae16e9ce8f9b74ddba3a87e49bfce4
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-09 17:09:51 +0300

    Added a new task
    Triples of Latin letters

---
+++ CSharp-Programming-Fundamentals/Exercise/02.1.dataTypesAndVariables-Exercise/06.triplesOfLatinLetters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _06.triplesOfLatinLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte n = byte.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                char firsChar = (char)('a' + i);
+                for (int j = 0; j < n; j++)
+                {
+                    char secondChar = (char)('a' + j);
+                    for (int k=0; k < n ; k++)
+                    {
+                        char thirdChar = (char)('a' + k);
+                        Console.WriteLine($"{firsChar}{secondChar}{thirdChar}");
+                    }
+                }
+            }
+        }
+    }
+}
