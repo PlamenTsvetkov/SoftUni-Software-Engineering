8949e6720f61ff033c0d0b2d4e8c695878356ada
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-10 16:32:47 +0300

    Added a new task
    Beer kegs

---
+++ CSharp-Programming-Fundamentals/Exercise/02.1.dataTypesAndVariables-Exercise/08.beerKegs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _08.beerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte n = byte.Parse(Console.ReadLine());
+            double maxValue = int.MinValue;
+            string maxModel = "";
+            for (int i = 0; i < n; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double volume = Math.PI * Math.Pow(radius, 2) * height;
+                if (volume>maxValue)
+                {
+                    maxModel = model;
+                    maxValue = volume;
+                }
+            }
+            Console.WriteLine(maxModel);
+        }
+    }
+}
