b7e4667de53de566e7bda85116fd98b65999d909
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-10 16:33:44 +0300

    Added a new task
    Spice must flow

---
+++ CSharp-Programming-Fundamentals/Exercise/02.1.dataTypesAndVariables-Exercise/09.spiceMustFlow/spiceMustFlow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _09.spiceMustFlow
+{
+    class spiceMustFlow
+    {
+        static void Main(string[] args)
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+            int days = 0;
+            int totalAmount = 0;
+            if (startingYield>=100)
+            {
+                while (startingYield >= 100)
+                {
+                    days += 1;
+                    totalAmount += startingYield - 26;
+                    startingYield -= 10;
+                }
+                totalAmount -= 26;                
+            }
+            Console.WriteLine(days);
+            Console.WriteLine(totalAmount);
+
+        }
+    }
+}
