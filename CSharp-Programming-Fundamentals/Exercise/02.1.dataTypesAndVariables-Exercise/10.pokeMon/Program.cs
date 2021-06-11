ee06409bbaaf53cc8cf50c8001fde83a82828f84
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-11 10:35:46 +0300

    Added a new task
    Poke mon

---
+++ CSharp-Programming-Fundamentals/Exercise/02.1.dataTypesAndVariables-Exercise/10.pokeMon/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Numerics;
+
+namespace _10.pokeMon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            byte y = byte.Parse(Console.ReadLine());
+
+            double originalN = n;
+            int target = 0;
+            while (n>=m)
+            {
+                if (n==originalN/2 && y>0)
+                {
+                    n /= y;
+
+                    if (n < m)
+                    {
+                        break;
+                    }
+                }
+                n -= m;
+                target++;
+            }
+            Console.WriteLine(n);
+            Console.WriteLine(target);
+        }
+    }
+}
