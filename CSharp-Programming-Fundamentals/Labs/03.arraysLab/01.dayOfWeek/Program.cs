c3b4737c2bb97b7995f541d890673ea670a8b1be
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-12 22:28:27 +0300

    Added a new task
    Day of week

---
+++ CSharp-Programming-Fundamentals/Labs/03.arraysLab/01.dayOfWeek/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01.dayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = new string[]{"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+            int number = int.Parse(Console.ReadLine());
+            if (number<=7 && number >0)
+            {
+                Console.WriteLine(days[number-1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+            
+        }
+    }
+}
