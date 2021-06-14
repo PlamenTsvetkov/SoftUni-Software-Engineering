271bdb5784a10dca7325d21d683309f4d853dc85
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-14 09:55:48 +0300

    Added a new task
    Reverse array of strings

---
+++ CSharp-Programming-Fundamentals/Labs/03.arraysLab/04.reverseArrayOfStrings2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.reverseArrayOfStrings2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine(string.Join(' ', arr.Reverse()));
+        }
+    }
+}
