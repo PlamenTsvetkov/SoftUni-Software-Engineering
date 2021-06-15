1671885d03efb938c234b171b31476aba0aa4d4a
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-15 22:34:10 +0300

    Added a new task
    Even and odd subtraction

---
+++ CSharp-Programming-Fundamentals/Labs/03.arraysLab/06.EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.EvenAndOddSubtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int evenSum = 0;
+            int oddSum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i]%2==0)
+                {
+                    evenSum += array[i];
+                }
+                else
+                {
+                    oddSum += array[i];
+                }
+            }
+            int result = evenSum - oddSum;
+            Console.WriteLine(result);
+        }
+    }
+}
