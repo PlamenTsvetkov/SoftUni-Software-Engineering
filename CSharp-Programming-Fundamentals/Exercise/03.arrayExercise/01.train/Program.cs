3bb0ed487ffc46f75169b314f992228dc7c2d2f8
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-16 09:59:23 +0300

    Added a new task
    Train

---
+++ CSharp-Programming-Fundamentals/Exercise/03.arrayExercise/01.train/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01.train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] train = new int[n];
+            int total = 0;
+            for (int i = 0; i < n; i++)
+            {
+                train[i] =int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < train.Length; i++)
+            {
+                total += train[i];
+            }
+            Console.WriteLine(string.Join(' ',train));
+            Console.WriteLine(total);
+        }
+    }
+}
