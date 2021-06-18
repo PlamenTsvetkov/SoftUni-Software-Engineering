b1c8ec25be2dc8119a48cccdae9ab54a7956c8f2
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-19 00:44:21 +0300

    Added a new task
    Top integers

---
+++ CSharp-Programming-Fundamentals/Exercise/03.arrayExercise/05.topIntegers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.topIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool isTrue = true;
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if (arr[j]>=arr[i])
+                    {
+                        isTrue = false;
+                    }
+                }
+                if (isTrue)
+                {
+                    Console.Write($"{arr[i]} ");
+                }
+            }
+        }
+    }
+}
