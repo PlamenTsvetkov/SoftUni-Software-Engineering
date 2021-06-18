fd1d4afb7b7ddbe9db2215852506e31ab6987631
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-18 15:52:17 +0300

    Added a new task
    Common elements

---
+++ CSharp-Programming-Fundamentals/Exercise/03.arrayExercise/02.commonElements/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02.commonElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringArr1 = Console.ReadLine().Split();
+            string[] stringArr2 = Console.ReadLine().Split();
+            for (int i = 0; i < stringArr2.Length; i++)
+            {
+                for (int j = 0; j < stringArr1.Length; j++)
+                {
+                    if (stringArr2[i]==stringArr1[j])
+                    {
+                        Console.Write($"{stringArr2[i]} ");
+                    }
+                }
+            }
+        }
+    }
+}
