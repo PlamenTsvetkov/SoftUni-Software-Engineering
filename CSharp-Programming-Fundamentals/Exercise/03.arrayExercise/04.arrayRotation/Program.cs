8b6c3ac47907d1c54c28dbe154c17f4dd743fcb0
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-19 00:42:24 +0300

    Added a new task
    Array rotation

---
+++ CSharp-Programming-Fundamentals/Exercise/03.arrayExercise/04.arrayRotation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04.arrayRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+            for (int j = 0; j < n; j++)
+            {
+                var end = arr[0];
+                for (var i = 0; i < arr.Length - 1; i++)
+                {
+                    arr[i] = arr[i + 1];
+                }
+                arr[arr.Length - 1] = end;
+            }
+            Console.WriteLine(string.Join(" ", arr));
+
+        }
+    }
+}
