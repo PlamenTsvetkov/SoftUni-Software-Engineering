c50927ca8781986f94b8f01ff0bb6eb039c1ed87
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-20 13:16:35 +0300

    Added a new task
    Max sequence of equal elements

---
+++ CSharp-Programming-Fundamentals/Exercise/03.arrayExercise/07.maxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.maxSequenceOfEqualElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+               .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToArray();
+            string sequence = string.Empty;
+            string maxsequence = string.Empty;
+            int maxCounter = int.MinValue;
+            int counter = 0;
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                
+                if (arr[i] == arr[i + 1])
+                {
+                    counter++;
+                    sequence = arr[i].ToString();
+                }
+                else
+                {
+                    counter = 0;
+                }
+                if (counter > maxCounter)
+                {
+                    maxCounter = counter;
+                    maxsequence = sequence;
+                }
+            }
+            for (int i = 0; i <= maxCounter; i++)
+            {
+                Console.Write($"{maxsequence} ");
+            }
+        }
+    }
+}
