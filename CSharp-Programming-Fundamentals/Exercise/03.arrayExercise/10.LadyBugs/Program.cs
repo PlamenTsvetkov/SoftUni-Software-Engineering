a051d5db161505df0d11253cb2963065f40cd5b9
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-22 18:32:17 +0300

    Added a new task
    Lady bugs

---
+++ CSharp-Programming-Fundamentals/Exercise/03.arrayExercise/10.LadyBugs/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace _10.LadyBugs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fieldSize = int.Parse(Console.ReadLine());
+            int[] initialIndexex = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int[] field = new int[fieldSize];
+
+            for (int i = 0; i < initialIndexex.Length; i++)
+            {
+                int currentIndex = initialIndexex[i];
+                if (currentIndex>=0 && currentIndex<fieldSize)
+                {
+                    field[currentIndex] = 1;
+                }
+
+            }
+            string comand = string.Empty;
+            while ((comand =Console.ReadLine())!="End")
+            {
+                string[] element = comand.Split();
+                int ladybugIndex = (int.Parse)(element[0]);
+                string direction = element[1];
+                int flyLenght = int.Parse(element[2]);
+                if (ladybugIndex < 0 || ladybugIndex > field.Length-1 || field[ladybugIndex] == 0);
+                {
+                    continue;
+                }
+                field[ladybugIndex] = 0;
+                if (direction=="right")
+                {
+                    
+                }
+                else if (direction=="left")
+                {
+
+                }
+            }
+        }
+    }
+}
