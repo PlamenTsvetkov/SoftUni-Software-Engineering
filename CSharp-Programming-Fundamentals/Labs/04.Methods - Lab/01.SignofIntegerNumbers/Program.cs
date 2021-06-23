29b07cc1115fe80e55f9e63feee5a02713ede61f
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-23 17:26:28 +0300

    Added a new task
    Sign of integer number

---
+++ CSharp-Programming-Fundamentals/Labs/04.Methods - Lab/01.SignofIntegerNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _01.SignofIntegerNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SignNumber(n);
+
+        }
+        static void SignNumber(int number)
+        {
+            if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else if (number>0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+        }
+    }
+}
