39114d53df5c8f7ec2ed5bf6b9006671a10040aa
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-24 18:17:50 +0300

    Added a new task
    Calculations

---
+++ CSharp-Programming-Fundamentals/Labs/04.Methods - Lab/03.Calculations/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _03.Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comand = Console.ReadLine();
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            switch (comand)
+            {
+                case "add":
+                    PrintAdd(a,b);
+                    break;
+                case "multiply":
+                    PrintMultiply(a,b);
+                    break;
+                case "substract":
+                    PrintSubstract(a,b);
+                    break;
+                case "divide":
+                    PrintDivide(a,b);
+                    break;
+                default:
+                    break;
+            }
+           
+            
+        }
+
+        private static void PrintDivide(int a, int b)
+        {
+            Console.WriteLine(a/b);
+        }
+
+        private static void PrintSubstract(int a, int b)
+        {
+            Console.WriteLine(a-b);
+        }
+
+        private static void PrintMultiply(int a, int b)
+        {
+            Console.WriteLine(a*b);
+        }
+
+        private static void PrintAdd(int a, int b)
+        {
+            Console.WriteLine(a+b);
+        }
+    }
+}
