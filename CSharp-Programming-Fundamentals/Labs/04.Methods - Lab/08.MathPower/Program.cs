a88c9e9b403b2739db1a2ba91e6c4781a6667789
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-26 11:52:58 +0300

    Added a new task
    Math power

---
+++ CSharp-Programming-Fundamentals/Labs/04.Methods - Lab/08.MathPower/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _08.MathPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double a =double.Parse(Console.ReadLine());
+           double b =double.Parse(Console.ReadLine());
+            double result = GetPow(a, b);
+            Console.WriteLine(result);
+        }
+
+        private static double GetPow(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+    }
+}
