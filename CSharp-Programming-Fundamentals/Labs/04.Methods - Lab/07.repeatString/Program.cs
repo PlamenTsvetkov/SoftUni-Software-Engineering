6f3c252999fe876f49935b0b941e2df2130b09bf
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-26 11:52:01 +0300

    Added a new task
    Repeat string

---
+++ CSharp-Programming-Fundamentals/Labs/04.Methods - Lab/07.repeatString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace _07.repeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            string result = GetString(str, n);
+            Console.WriteLine(result);
+        }
+
+        private static string GetString(string str, int n)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                result.Append(str);
+            }
+            return result.ToString();
+        }
+    }
+}
