7fa3ac8823a615908e7eeda1b4b9019f64f73702
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-27 14:30:18 +0300

    Added a new task
    Multiply evens by odds

---
+++ CSharp-Programming-Fundamentals/Labs/04.Methods - Lab/10.MultiplyEvensby Odds/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _10.MultiplyEvensbyOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Math.Abs(int.Parse(Console.ReadLine()));
+
+            int result = GetMultipleOfEvenAndOdds(a);
+            Console.WriteLine(result);
+        }
+
+        private static int GetMultipleOfEvenAndOdds(int a)
+        {
+            int result = GetSumOfEvenDigits(a)*GetSumOfOddDigits(a);
+            return result;
+        }
+
+        private static int GetSumOfOddDigits(int a)
+        {
+            int result = 0;
+            int currentSymvol = 0;
+            while (a != 0)
+            {
+                currentSymvol = a % 10;
+                if (currentSymvol % 2 == 1)
+                {
+                    result += currentSymvol;
+                }
+                a /= 10;
+
+            }
+            return result;
+        }
+
+        private static int GetSumOfEvenDigits(int a)
+        {
+            int result = 0;
+            int currentSymvol = 0;
+            while (a!=0)
+            {
+                currentSymvol = a % 10;
+                if (currentSymvol%2==0)
+                {
+                    result += currentSymvol;
+                }
+                a /= 10;
+
+            }
+            return result;
+        }
+
+        
+    }
+}
