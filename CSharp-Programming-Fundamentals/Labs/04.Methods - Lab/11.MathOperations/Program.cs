011099ae92d7be3370f4284ea1a9703d8158e83b
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-28 10:04:44 +0300

    Added a new task
    Math operations

---
+++ CSharp-Programming-Fundamentals/Labs/04.Methods - Lab/11.MathOperations/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _11.MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            char operators = char.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            double result = Calculate(num1, operators, num2);
+            Console.WriteLine(result);
+
+        }
+
+        private static double Calculate(int num1, char operators, int num2)
+        {
+            double result = 0;
+            if (operators=='/')
+            {
+                result = num1 / num2;
+            }
+            else if (operators == '*')
+            {
+                result = num1 * num2;
+            }
+            else if (operators == '-')
+            {
+                result = num1 - num2;
+            }
+            else if (operators == '+')
+            {
+                result = num1 + num2;
+            }
+            return result;
+        }
+    }
+}
