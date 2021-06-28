0ab35a1618b243cf45ab7458619201646ca65c98
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-28 10:06:54 +0300

    Added a new task
    Smallest of three numbers

---
+++ CSharp-Programming-Fundamentals/Exercise/04.MethodsExercise/01.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01.SmallestOfThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int result = GetBiggestNumber(a, b, c);
+            Console.WriteLine(result);
+        }
+
+        private static int GetBiggestNumber(int a, int b, int c)
+        {
+            return Math.Min(c, Math.Min(a, b));
+        }
+    }
+}
