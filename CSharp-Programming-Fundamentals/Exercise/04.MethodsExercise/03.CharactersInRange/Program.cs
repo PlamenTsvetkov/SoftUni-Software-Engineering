304210a95db0bbce4a9035edcbab73ee8d8b9766
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-29 17:41:42 +0300

    Added a new task
    Characters in range

---
+++ CSharp-Programming-Fundamentals/Exercise/04.MethodsExercise/03.CharactersInRange/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace _03.CharactersInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char cha1 = char.Parse(Console.ReadLine());
+            char cha2 = char.Parse(Console.ReadLine());
+            PrintCharacterBetweenTwoChar(cha1,cha2);
+        }
+
+         static void PrintCharacterBetweenTwoChar(char cha1, char cha2)
+        {
+            if (cha1<cha2)
+            {
+                for (int i = cha1 + 1; i < cha2; i++)
+                {
+                    Console.Write(($"{(char)i} "));
+                }
+            }
+            else
+            {
+                for (int i = cha2 + 1; i < cha1; i++)
+                {
+                    Console.Write(($"{(char)i} "));
+                }
+            }
+           
+            
+        }
+    }
+}
