24582db902f7cc8df1cd1c2c965838c347fc1cd9
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-06-29 17:40:41 +0300

    Added a new task
    Vowels count

---
+++ CSharp-Programming-Fundamentals/Exercise/04.MethodsExercise/02.VowelsCount/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _02.VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine().ToLower();
+            int vowelsNum = CountCowels(word);
+            Console.WriteLine(vowelsNum);
+        }
+
+        private static int CountCowels(string word)
+        {
+            int vowelsCount = 0;
+            for (int i = 0; i < word.Length; i++) 
+            {
+                switch (word[i])
+                {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        vowelsCount++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return vowelsCount;
+        }
+    }
+}
