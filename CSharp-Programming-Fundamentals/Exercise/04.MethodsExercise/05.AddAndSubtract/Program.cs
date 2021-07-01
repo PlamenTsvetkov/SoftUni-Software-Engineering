e3c056eb82f2c2b61e9f7d02f4f664fe8172b358
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-07-01 10:43:06 +0300

    Added a new task
    Add and subtract

---
+++ CSharp-Programming-Fundamentals/Exercise/04.MethodsExercise/05.AddAndSubtract/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _05.AddAndSubtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            SumAB(a, b,c);
+        }
+
+        private static void  SumAB(int a, int b, int c)
+        {
+            Console.WriteLine((a + b) - c);
+        }
+    }
+}
