b8a35f9c8fc2871a4be0a048eafc7cf890e6f5cb
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-07-01 10:44:57 +0300

    Added a new task
    Middle characters

---
+++ CSharp-Programming-Fundamentals/Exercise/04.MethodsExercise/06.MiddleCharacters/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _06.MiddleCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            GetMiddleCharacter(word);
+        }
+
+        private static void GetMiddleCharacter(string word)
+        {
+            string chars = string.Empty;
+            if (word.Length%2==0)
+            {
+                for (int i = (word.Length / 2) - 1; i <= (word.Length / 2); i++)
+                {
+                    chars += word[i].ToString();
+                }
+                
+            }
+            else
+            {
+                for (int i = word.Length / 2; i < (word.Length / 2) + 1; i++)
+                {
+                    chars = word[i].ToString();
+                }
+            }
+            Console.WriteLine(chars);
+        }
+    }
+}
