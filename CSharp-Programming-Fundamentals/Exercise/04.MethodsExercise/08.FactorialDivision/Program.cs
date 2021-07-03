3dcf1f640da69c57ccf5ecdf96ed01eb5cf62746
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-07-04 00:21:07 +0300

    Added a new task
    Factorial division

---
+++ CSharp-Programming-Fundamentals/Exercise/04.MethodsExercise/08.FactorialDivision/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _08.FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            FactorialDivision(a, b);
+        }
+
+        private static void FactorialDivision(double a, double b)
+        {
+            double sumFactorialA = 1;
+            double sumFactorialB = 1;
+            for (int i = 1; i <= a; i++)
+            {
+                sumFactorialA *= i;
+            }
+            for (int i = 1; i <= b; i++)
+            {
+                sumFactorialB *= i;
+            }
+            double result = sumFactorialA / sumFactorialB;
+            Console.WriteLine($"{result:f2}");
+        }
+    }
+}
