ac8040dcbec6de3cffe784de07aaa20f7aaa0333
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-07-07 00:51:07 +0300

    Added a new task
    List of products

---
+++ CSharp-Programming-Fundamentals/Labs/05.List-Lab/04.ListofProducts/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.ListofProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> productList = new List<string>(n);
+            for (int i = 0; i < n; i++)
+            {
+                productList.Add(Console.ReadLine());
+            }
+            productList.Sort();
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{i+1}.{productList[i]}");
+            }
+        }
+    }
+}
