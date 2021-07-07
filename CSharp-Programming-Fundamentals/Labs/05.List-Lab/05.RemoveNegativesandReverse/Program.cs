2e3b567b8e32708097ea38bde17db0bc5be1dde3
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-07-08 01:30:32 +0300

    Added a new task
    Remove negatives and reverse

---
+++ CSharp-Programming-Fundamentals/Labs/05.List-Lab/05.RemoveNegativesandReverse/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.RemoveNegativesandReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list1 = Console.ReadLine()
+               .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToList();
+            List<int> result = list1
+                .Where(n => n >= 0)
+                .Reverse()
+                .ToList();
+            if (result.Count==0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ",result));
+            }
+        }
+    }
+}
