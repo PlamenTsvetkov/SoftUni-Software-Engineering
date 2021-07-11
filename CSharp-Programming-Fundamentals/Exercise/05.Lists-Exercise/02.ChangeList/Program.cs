41150ed8a2bbdcbad31076135debbce158f40c4c
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-07-11 22:13:54 +0300

    Added a new task
    Change list

---
+++ CSharp-Programming-Fundamentals/Exercise/05.Lists-Exercise/02.ChangeList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.ChangeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lists = Console.ReadLine()
+              .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+              .Select(int.Parse)
+              .ToList();
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                List<string> comand = input.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+                if (comand[0]=="Delete")
+                {
+                    while (lists.Remove(int.Parse(comand[1])))
+                    {
+                        lists.Remove(int.Parse(comand[1]));
+                    }
+                }
+                else if (comand[0] == "Insert")
+                {
+                    lists.Insert(int.Parse(comand[2]), int.Parse(comand[1]));
+                }
+            }
+            Console.WriteLine(string.Join(" ", lists));
+        }
+    }
+}
