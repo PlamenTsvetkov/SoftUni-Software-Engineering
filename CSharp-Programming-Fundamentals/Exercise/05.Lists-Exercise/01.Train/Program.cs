8ece5290b6cf3b62d73ee42b61196b69fcc35b78
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-07-11 22:12:23 +0300

    Added a new task
    Train

---
+++ CSharp-Programming-Fundamentals/Exercise/05.Lists-Exercise/01.Train/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine()
+               .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToList();
+            int capacity = int.Parse(Console.ReadLine());
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                List<string> comand = input.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+                if (comand[0]=="Add")
+                {
+                    wagons.Add(int.Parse(comand[1]));
+                }
+                else
+                {
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        if (capacity-wagons[i]>=int.Parse(comand[0]))
+                        {
+                            wagons[i] += int.Parse(comand[0]);
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
