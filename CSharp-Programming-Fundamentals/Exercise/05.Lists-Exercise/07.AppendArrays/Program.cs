88c1d6196999386cd80ad625c74909c21a445c8f
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-07-14 17:50:13 +0300

    Added a new task
    Append arrays

---
+++ CSharp-Programming-Fundamentals/Exercise/05.Lists-Exercise/07.AppendArrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.AppendArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> massivList = Console.ReadLine()
+            .Split('|', StringSplitOptions.RemoveEmptyEntries)
+            .Reverse()
+            .ToList();
+            for (int i = 0; i < massivList.Count; i++)
+            {
+                List<string> newList = massivList[i]
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+                for (int j = 0; j < newList.Count; j++)
+                {
+                    Console.Write($"{newList[j]} ");
+                }
+            }
+            
+        }
+    }
+}
