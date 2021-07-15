9021575b14fd9f6157cb5f8634a8ae1724ff2d1e
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-07-15 17:27:40 +0300

    Added a new task
    Day of week

---
+++ CSharp-Programming-Fundamentals/Labs/06.Objects and Classes - Lab/01.DayOfWeek/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace _01.DayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataString = Console.ReadLine();
+            DateTime date = DateTime.ParseExact(dataString, "d-M-yyyy", CultureInfo.InvariantCulture);
+            Console.WriteLine(date.DayOfWeek);
+            
+        }
+    }
+}
