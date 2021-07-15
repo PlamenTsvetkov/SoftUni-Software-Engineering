4050f15214ca67574fa64b5849a9ba7f3bcda757
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-07-15 17:26:29 +0300

    Added a new task
    Randomise words

---
+++ CSharp-Programming-Fundamentals/Labs/06.Objects and Classes - Lab/01. Randomize Words/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Randomize_Words
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine().Split().ToArray();
+            Random rnd = new Random();
+            for (int i = 0; i < text.Length; i++)
+            {
+                int timelyPoin = rnd.Next(0, text.Length);
+                string currentPoint = text[i];
+                text[i] = text[timelyPoin];
+                text[timelyPoin] = currentPoint;
+                
+            }
+            Console.WriteLine(string.Join(Environment.NewLine,text));
+        }
+
+    }
+}
