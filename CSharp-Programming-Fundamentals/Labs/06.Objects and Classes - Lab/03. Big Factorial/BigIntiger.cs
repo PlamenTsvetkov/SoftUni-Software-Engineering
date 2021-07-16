84189206c0a53c0728a8a8a5017de4e300261635
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-07-16 18:04:28 +0300

    Added a new task
    Big integer

---
+++ CSharp-Programming-Fundamentals/Labs/06.Objects and Classes - Lab/03. Big Factorial/BigIntiger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03._Big_Factorial
+{
+    public class BigIntiger
+    {
+        public static implicit operator BigIntiger(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CSharp-Programming-Fundamentals/Labs/06.Objects and Classes - Lab/03. Big Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+
+namespace _03._Big_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(BigFacturial(n));
+        }
+        public static BigInteger BigFacturial(int n)
+        {
+            BigInteger result = 1;
+            for (int i = 1; i <=n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
