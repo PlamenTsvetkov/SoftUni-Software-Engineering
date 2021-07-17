940a7c1ee9975ce9a18112ba177b29a7b0078114
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-07-18 00:24:20 +0300

    Added a new task
    Students

---
+++ CSharp-Programming-Fundamentals/Labs/06.Objects and Classes - Lab/05. Students/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            List<Student> students = new List<Student>();
+            while ((input = Console.ReadLine())!="end")
+            {
+                List<string> curentStudent = input
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+                Student student = new Student(curentStudent[0], curentStudent[1], int.Parse(curentStudent[2]), curentStudent[3]);
+                students.Add(student);
+
+            }
+            string currentTown = Console.ReadLine();
+
+            Console.WriteLine(string.Join(Environment.NewLine,students.Where(x=>x.HomeTown==currentTown)));
+        }
+        public class Student
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Age { get; set; }
+            public string HomeTown { get; set; }
+            public Student(string firstName, string lastName, int age, string homeTown)
+            {
+                this.FirstName = firstName;
+                this.LastName = lastName;
+                this.Age = age;
+                this.HomeTown = homeTown;
+            }
+            public override string ToString()
+            {
+                return $"{FirstName} {LastName} is {Age} years old."; 
+            }
+        }
+
+    }
+}
