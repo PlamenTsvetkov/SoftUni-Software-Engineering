1d7988c013f3b5765215d5261d2c41db2aa86ce2
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-07-21 09:21:53 +0300

    Added a new task
    Students

---
+++ CSharp-Programming-Fundamentals/Exercise/06.Objects and Classes - Exercise/04. Students/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Students> students = new List<Students>();
+            for (int i = 0; i < n; i++)
+            {
+                var studentInfo = Console.ReadLine()
+                    .Split(" ")
+                    .ToArray();
+                Students student = new Students(studentInfo[0], studentInfo[1], double.Parse(studentInfo[2]));
+                students.Add(student);
+            }
+            
+            Console.WriteLine(string.Join(Environment.NewLine, students.OrderByDescending(x => x.Grade)));
+        }
+        public class Students
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public double Grade { get; set; }
+            public Students(string firstName, string lastName, double grade)
+            {
+                this.FirstName = firstName;
+                this.LastName = lastName;
+                this.Grade = grade;
+            }
+            public override string ToString()
+            {
+                return $"{FirstName} {LastName}: {Grade:f2}";
+            }
+        }
+    }
+}
+
