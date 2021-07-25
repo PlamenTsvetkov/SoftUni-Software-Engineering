5a75c282fa446c263613cb792ab17a8eb28820e0
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-07-25 23:35:23 +0300

    Added a new task
    Count Real Numbers

---
+++ CSharp-Programming-Fundamentals/Labs/07.Associative Arrays - Lab/01. Count Real Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] number = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            SortedDictionary<double, int> count = new SortedDictionary<double, int>();
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (count.ContainsKey(number[i]))
+                {
+                    count[number[i]]++;
+                }
+                else
+                {
+                    count.Add(number[i], 1);
+                }
+            }
+            foreach (var item in count)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
