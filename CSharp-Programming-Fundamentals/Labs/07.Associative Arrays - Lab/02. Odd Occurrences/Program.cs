0d4ebd1f99aef5648ae96c2630926f9e60bee577
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-07-25 23:38:22 +0300

    Added a new task
    Odd Occurrences

---
+++ CSharp-Programming-Fundamentals/Labs/07.Associative Arrays - Lab/02. Odd Occurrences/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Odd_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sequence= Console.ReadLine().ToLower()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            Dictionary<string, int> oddWord = new Dictionary<string, int>();
+
+            foreach (var word in sequence)
+            {
+                if (oddWord.ContainsKey(word))
+                {
+                    oddWord[word]++;
+                }
+                else
+                {
+                    oddWord.Add(word, 1);
+                }
+            }
+            foreach (var pair in oddWord)
+            {
+                if (pair.Value%2==1)
+                {
+                    Console.Write($"{pair.Key} ");
+                }
+            }
+        }
+    }
+}
