a0191e378a837a43cb9cbf657bfda0fcc2c3813b
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-07-26 21:38:10 +0300

    Added a new task
     Largest 3 Numbers

---
+++ CSharp-Programming-Fundamentals/Labs/07.Associative Arrays - Lab/04. Largest 3 Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intiger = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            intiger = intiger.OrderByDescending(x => x).ToList();
+            for (int i = 0; i < 3; i++)
+            {
+                if (i<intiger.Count)
+                {
+                    Console.Write($"{intiger[i]} ");
+                }
+                else
+                {
+                    break;
+                }
+                
+            }
+            
+        }
+    }
+}
