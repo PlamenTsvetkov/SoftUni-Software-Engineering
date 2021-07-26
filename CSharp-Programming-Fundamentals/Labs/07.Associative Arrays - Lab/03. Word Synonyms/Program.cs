79e800b156023b3a4ad1d883dba6e8dc0aebafbe
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-07-26 21:36:58 +0300

    Added a new task
    Word synonyms

---
+++ CSharp-Programming-Fundamentals/Labs/07.Associative Arrays - Lab/03. Word Synonyms/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03._Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<string>> dictionary = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i <n; i++)
+            {
+                string newKey = Console.ReadLine();
+                string newValue =Console.ReadLine();
+                if (!dictionary.ContainsKey(newKey))
+                {
+                    dictionary.Add(newKey, new List<string> { newValue });
+                }
+                else
+                {
+                    dictionary[newKey].Add(newValue);
+                }
+            }
+            foreach (var pair in dictionary)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append($"{pair.Key} - {string.Join(", ", pair.Value)}").ToString();
+                Console.WriteLine(sb);
+            }
+        }
+    }
+}
