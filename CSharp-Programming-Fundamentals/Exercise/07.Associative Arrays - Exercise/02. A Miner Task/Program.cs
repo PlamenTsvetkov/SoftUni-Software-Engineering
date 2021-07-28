12e4f1108c910bec7022cb4120ab7273302b0717
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-07-28 22:08:53 +0300

    Added a new task
    A Miner Task

---
+++ CSharp-Programming-Fundamentals/Exercise/07.Associative Arrays - Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            Dictionary<string, int> minetTask = new Dictionary<string, int>();
+            while ((input = Console.ReadLine())!="stop")
+            {
+                string keys = input;
+                int value = int.Parse(Console.ReadLine());
+                if (!minetTask.ContainsKey(keys))
+                {
+                    minetTask.Add(keys, value);
+                }
+                else
+                {
+                    minetTask[keys] += value;
+                }
+                
+            }
+            foreach (var pair in minetTask)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
