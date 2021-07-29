823428b4e12ce9e3267b9080c7c48211daef2256
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-07-29 23:40:23 +0300

    Orders
    Added a new task

---
+++ CSharp-Programming-Fundamentals/Exercise/07.Associative Arrays - Exercise/04. Orders/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            Dictionary<string, PriceStock> products = new Dictionary<string, PriceStock>();
+            while ((input = Console.ReadLine())!="buy")
+            {
+                List<string> product = input
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+                if (!products.ContainsKey(product[0]))
+                {
+                    PriceStock priceStock = new PriceStock(double.Parse(product[1]), int.Parse(product[2]));
+                    products.Add(product[0], priceStock);
+                }
+                else
+                {
+                    products[product[0]].Stock += int.Parse(product[2]);
+                    products[product[0]].Price = double.Parse(product[1]);
+                }
+
+            }
+            foreach (var pair in products)
+            {
+                Console.WriteLine($"{pair.Key} -> {products[pair.Key].Stock * products[pair.Key].Price:f2}");
+            }
+         
+        }
+        class PriceStock
+        {
+            public double Price { get; set; }
+            public int Stock { get; set; }
+            public PriceStock(double price, int stock )
+            {
+                this.Price = price;
+                this.Stock = stock;
+            }
+        }
+    }
+}
