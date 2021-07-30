379e51b4ef45b1570cc92a85c2e75c9fa15b0838
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-07-30 13:03:34 +0300

    Student Academy
    Added a new task

---
+++ CSharp-Programming-Fundamentals/Exercise/07.Associative Arrays - Exercise/07. Student Academy/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Student_Academy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> studentGrade = new Dictionary<string, List<double>>();
+            for (int i = 0; i < n; i++)
+            {
+                string studentName = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!studentGrade.ContainsKey(studentName))
+                {
+                    studentGrade.Add(studentName, new List<double> {grade});
+                }
+                else
+                {
+                    studentGrade[studentName].Add(grade);
+                }
+            }
+            foreach (var pair in studentGrade.OrderByDescending(x=>x.Value.Average()))
+            {
+                if (pair.Value.Average()>=4.5)
+                {
+                    Console.WriteLine($"{pair.Key} -> {pair.Value.Average():f2}");
+                }
+            }
+            
+
+        }
+    }
+}
