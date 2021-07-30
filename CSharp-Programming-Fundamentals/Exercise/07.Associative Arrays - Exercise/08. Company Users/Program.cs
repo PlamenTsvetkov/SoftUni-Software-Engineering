c4df98269b7f09b1b777c298cdd49d8f34de48db
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-07-31 00:01:48 +0300

    Company Users
    Added a new task

---
+++ CSharp-Programming-Fundamentals/Exercise/07.Associative Arrays - Exercise/08. Company Users/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Company_Users
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            Dictionary<string, List<string>> companyID = new Dictionary<string, List<string>>();
+            while ((input = Console.ReadLine()) != "End")
+            {
+                List<string> inputData = input
+                    .Split(" -> ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+                if (!companyID.ContainsKey(inputData[0]))
+                {
+                    companyID.Add(inputData[0], new List<string> { inputData[1] });
+                }
+                else
+                {
+                    if (!companyID[inputData[0]].Contains(inputData[1]))
+                    {
+                        companyID[inputData[0]].Add(inputData[1]);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+                }
+            }
+            foreach (var pair in companyID.OrderBy(pair => pair.Key))
+            {
+                Console.WriteLine($"{pair.Key}");
+                foreach (var ID in pair.Value)
+                {
+                    Console.WriteLine($"-- {ID}");
+                }
+            }
+        }
+    }
+}
