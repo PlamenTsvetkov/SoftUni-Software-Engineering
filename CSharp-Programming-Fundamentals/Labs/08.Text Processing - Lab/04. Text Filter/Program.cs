d3fa23292884877355e7e09b2796f4755ca71deb
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-08-02 09:40:07 +0300

    Text Filter
    Added a new task

---
+++ CSharp-Programming-Fundamentals/Labs/08.Text Processing - Lab/04. Text Filter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] banList = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            string text = Console.ReadLine();
+            for (int i = 0; i < banList.Length; i++)
+            {
+                string replacement = new string('*', banList[i].Length);
+                text = text.Replace(banList[i], replacement);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
