6c0d51d610a13b0cfebd2a702b2b2b621f590d08
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-08-03 18:03:12 +0300

    Repeat Strings
    Added a new task

---
+++ CSharp-Programming-Fundamentals/Labs/08.Text Processing - Lab/02. Repeat Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayString = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            for (int i = 0; i < arrayString.Length; i++)
+            {
+                for (int k = 0; k < arrayString[i].Length; k++)
+                {
+                    Console.Write(arrayString[i]);
+                }
+            }
+        }
+    }
+}
