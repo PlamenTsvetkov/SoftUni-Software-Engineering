bbde1ad6f748cfc7f2ac636dddd489232487eb96
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-08-05 16:48:26 +0300

    Caesar Cipher
    Added a new task

---
+++ CSharp-Programming-Fundamentals/Exercise/07.Text Processing - Exercise/04. Caesar Cipher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace _04._Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            StringBuilder changeText = new StringBuilder(text);
+            
+            for (int i = 0; i < changeText.Length; i++)
+            {
+                char currentSimvol = text[i];
+                currentSimvol += (char)3;
+                changeText[i] = currentSimvol;
+            }
+            Console.WriteLine(changeText);
+        }
+    }
+}
