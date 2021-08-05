444faa7afea973ee58bbe444b2807e7fba835bec
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-08-05 16:47:26 +0300

    Extract File
    Added a new task

---
+++ CSharp-Programming-Fundamentals/Exercise/07.Text Processing - Exercise/03. Extract File/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> pathFile = Console.ReadLine()
+                .Split("\\", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            List<string> fileNameExtension = pathFile[pathFile.Count - 1]
+                .Split(".", StringSplitOptions.RemoveEmptyEntries).ToList();
+            Console.WriteLine($"File name: {fileNameExtension[0]}");
+            Console.WriteLine($"File extension: {fileNameExtension[1]}");
+        }
+    }
+}
