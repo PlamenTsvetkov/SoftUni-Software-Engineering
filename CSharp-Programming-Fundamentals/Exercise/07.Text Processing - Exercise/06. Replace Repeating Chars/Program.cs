bc2e318b20302aa0c7de90dc5fe23ad2110bf052
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-08-06 17:23:02 +0300

    Replace Repeating Chars
    Added a new task

---
+++ CSharp-Programming-Fundamentals/Exercise/07.Text Processing - Exercise/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace _06._Replace_Repeating_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            StringBuilder stringText = new StringBuilder(text);
+            for (int i = 0; i < stringText.Length-1; i++)
+            {
+                if (stringText[i]==stringText[i+1])
+                {
+                    stringText.Remove(i, 1);
+                    i--;
+                }
+            }
+            Console.WriteLine(stringText);
+        }
+    }
+}
