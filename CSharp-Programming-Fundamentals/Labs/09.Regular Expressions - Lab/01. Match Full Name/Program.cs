ccc3f2598838bae38d28c155ab5521d3c48652d2
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-08-08 22:04:15 +0300

    Match Full Name
    Added a new task

---
+++ CSharp-Programming-Fundamentals/Labs/09.Regular Expressions - Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _01._Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+            Regex regex = new Regex(pattern);
+            string input = Console.ReadLine();
+            var result = regex.Matches(input);
+            foreach (Match match in result)
+            {
+                Console.Write($"{match.Value} ");
+            }
+        }
+    }
+}
