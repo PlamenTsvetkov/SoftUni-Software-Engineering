5d66fcfa0b774d246c2457f5ecac6bdfa0ea88e0
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-08-08 22:05:24 +0300

    Match Phone Number
    Added a new task

---
+++ CSharp-Programming-Fundamentals/Labs/09.Regular Expressions - Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//\b[+359]+([ ,-])2\1\d{3}\1\d{4}\b
+            string input = Console.ReadLine();
+            string pattern = @"\+359([\s-])[2]\1\d{3}\1\d{4}\b";
+            var phones = Regex.Matches(input, pattern);
+
+            Console.WriteLine(string.Join(", ", phones));
+        }
+    }
+}
