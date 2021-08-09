73169af1c033b887eacaddf3dcd5fd21ec5eaf50
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-08-09 17:27:33 +0300

    Furniture
    Added a new task

---
+++ CSharp-Programming-Fundamentals/Exercise/09.Regular Expressions - Exercise/01. Furniture/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _01._Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            List<string> furnitures = new List<string>();
+            double totalSum = 0;
+            while ((input = Console.ReadLine()) != "Purchase")
+            {
+
+                string patern = @">>(?<name>.+)<<(?<price>\d+.*\d*)!(?<quantity>\d+)";
+                var furniture = Regex.Match(input, patern);
+                if (furniture.Success)
+                {
+                    furnitures.Add(furniture.Groups["name"].Value);
+                    totalSum += double.Parse(furniture.Groups["price"].Value) * double.Parse(furniture.Groups["quantity"].Value);
+                }
+
+            }
+            Console.WriteLine("Bought furniture:");
+            if (furnitures.Count>0)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, furnitures));
+            }
+            
+            Console.WriteLine($"Total money spend: {totalSum:f2}");
+        }
+    }
+}
