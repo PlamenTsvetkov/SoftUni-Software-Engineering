fa8eb16aa49fcebe09f4cd7788a8f70edbc93473
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-08-11 15:20:41 +0300

    SoftUni Bar Income
    Added a new task

---
+++ CSharp-Programming-Fundamentals/Exercise/09.Regular Expressions - Exercise/03. SoftUni Bar Income/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._SoftUni_Bar_Income
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            double totalIncome = 0;
+            while ((input=Console.ReadLine())!= "end of shift")
+            {
+                string pattern = @"%(?<customer>[A-Z][a-z]+)%[^|$%.]*?<(?<product>\w+)>[^|$%.]*?\|(?<count>\d+)\|[^|$%.]*?(?<price>\d+\.*\d*)\$";
+                var regex = Regex.Match(input, pattern);
+                if (regex.Success)
+                {
+                    string customer = regex.Groups["customer"].Value;
+                    string product = regex.Groups["product"].Value;
+                    int count = int.Parse(regex.Groups["count"].Value);
+                    double price = double.Parse(regex.Groups["price"].Value);
+                    double totalPrice = count * price;
+                    Console.WriteLine($"{customer}: {product} - {totalPrice:f2}");
+                    totalIncome += totalPrice;
+                }
+            }
+            Console.WriteLine($"Total income: {totalIncome:f2}");
+        }
+    }
+}
