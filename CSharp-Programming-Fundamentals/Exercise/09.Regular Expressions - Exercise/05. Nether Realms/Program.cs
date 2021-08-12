223cb372a03062c436e4bba59f58de0202b612ec
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-08-12 17:56:33 +0300

    Nether Realms
    Added a new task

---
+++ CSharp-Programming-Fundamentals/Exercise/09.Regular Expressions - Exercise/05. Nether Realms/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _05._Nether_Realms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> demonNames = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            Dictionary<string, List<double>> demons = new Dictionary<string, List<double>>();
+            string patternHealth = @"[^\d+*\/.-]";
+            string patternDMG = @"[-+]?([0-9]*\.[0-9]+|[0-9]+)";
+            string patternComand = @"[\*\/]";
+            for (int i = 0; i < demonNames.Count; i++)
+            {
+                var regexHealth = Regex.Matches(demonNames[i], patternHealth);
+                double totalHealth = 0;
+                foreach (Match match in regexHealth)
+                {
+                    totalHealth += char.Parse(match.Value);
+                }
+                var regexDMG = Regex.Matches(demonNames[i], patternDMG);
+                double totalDmg = 0;
+                foreach (Match match in regexDMG)
+                {
+                    totalDmg += double.Parse(match.Value);
+                }
+                var regexComand = Regex.Matches(demonNames[i], patternComand);
+                foreach (Match match in regexComand)
+                {
+                    if (match.Value == "*")
+                    {
+                        totalDmg *= 2;
+                    }
+                    else
+                    {
+                        totalDmg /= 2;
+                    }
+                }
+                demons.Add(demonNames[i], new List<double> { totalHealth, totalDmg });
+            }
+            foreach (var item in demons.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{item.Key} - {item.Value[0]} health, {item.Value[1]:f2} damage");
+            }
+
+        }
+    }
+}
