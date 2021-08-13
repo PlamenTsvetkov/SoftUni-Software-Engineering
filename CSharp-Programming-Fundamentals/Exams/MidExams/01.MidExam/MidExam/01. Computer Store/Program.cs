11a2b8aa42e8862bb9b59395956ae2e20a92f05c
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-08-13 17:14:11 +0300

    Computer Store
    Added a new task

---
+++ CSharp-Programming-Fundamentals/Exams/MidExams/01.MidExam/MidExam/01. Computer Store/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _01._Computer_Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            double totalPriceWithoutTax = 0;
+            while ((input = Console.ReadLine()) != "special")
+            {
+                if (input== "regular")
+                {
+                    break;
+                }
+                double currentPrice = double.Parse(input);
+                if (currentPrice<0)
+                {
+                    Console.WriteLine("Invalid price!");
+                    continue;
+                }
+                totalPriceWithoutTax += currentPrice;
+            }
+            double tax = totalPriceWithoutTax * 0.2;
+            double totalPriceWithTax = totalPriceWithoutTax + tax;
+            if (input == "special")
+            {
+                totalPriceWithTax *= 0.9;
+            }
+            if (totalPriceWithTax==0)
+            {
+                Console.WriteLine("Invalid order!");
+            }
+            else
+            {
+                Console.WriteLine($"Congratulations you've just bought a new computer!");
+                Console.WriteLine($"Price without taxes: {totalPriceWithoutTax:f2}$");
+                Console.WriteLine($"Taxes: {tax:f2}$");
+                Console.WriteLine($"-----------");
+                Console.WriteLine($"Total price: {totalPriceWithTax:f2}$");
+            }
+
+        }
+    }
+}
