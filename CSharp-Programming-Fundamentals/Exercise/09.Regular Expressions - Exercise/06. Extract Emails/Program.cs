517a0b271bdc649f33080a69a5a8549a94057359
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-08-13 17:10:45 +0300

    Extract Emails
    Added a new task

---
+++ CSharp-Programming-Fundamentals/Exercise/09.Regular Expressions - Exercise/06. Extract Emails/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _06._Extract_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string pattern = @"(?<=\s)(?<user>[A-Za-z\d]+([\.,-][A-Za-z\d]+)*)@(?<host>[A-Za-z]+([\.-][A-Za-z]+)*\.[a-z]{2,}\b)";
+            var regex = Regex.Matches(text, pattern);
+            foreach (Match match in regex)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+    }
+}
