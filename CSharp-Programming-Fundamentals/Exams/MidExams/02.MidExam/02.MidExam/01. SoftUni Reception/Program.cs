22e45672f799a23375fdd495c7480e8356614ddf
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-08-15 22:37:27 +0300

    SoftUni Reception
    Added a new task

---
+++ CSharp-Programming-Fundamentals/Exams/MidExams/02.MidExam/02.MidExam/01. SoftUni Reception/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01._SoftUni_Reception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            byte firstEmployeeEfficiency = byte.Parse(Console.ReadLine());
+            byte secondEmployeeEfficiency = byte.Parse(Console.ReadLine());
+            byte thirdEmployeeEfficiency = byte.Parse(Console.ReadLine());
+            int studentsCount = int.Parse(Console.ReadLine());
+            int totalStudentsForHour = firstEmployeeEfficiency + secondEmployeeEfficiency + thirdEmployeeEfficiency;
+
+            int timeNeeded = 0;
+            while (studentsCount>0)
+            {
+                timeNeeded++;
+                studentsCount -= totalStudentsForHour;
+                if (timeNeeded%4==0)
+                {
+                    timeNeeded++;
+                }
+            }
+             
+            
+
+            Console.WriteLine($"Time needed: {timeNeeded}h.");
+        }
+    }
+}
