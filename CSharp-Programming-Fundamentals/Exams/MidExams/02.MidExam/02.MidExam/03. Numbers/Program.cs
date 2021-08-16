c334dfdf27e8d786e16a187789c7e978ab0466f8
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-08-16 09:47:59 +0300

    Numbers
    Added a new task

---
+++ CSharp-Programming-Fundamentals/Exams/MidExams/02.MidExam/02.MidExam/03. Numbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> theList = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .OrderByDescending(x=>x)
+                .ToList();
+            double average = theList.Average();
+            List<int> result = new List<int>();
+            for (int i = 0; i < theList.Count; i++)
+            {
+                if (theList[i]>average)
+                {
+                    result.Add(theList[i]);
+                }
+                if (result.Count==5)
+                {
+                    break;
+                }
+            }         
+            if (result.Count==0)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ",result));
+            }
+        }
+    }
+}
