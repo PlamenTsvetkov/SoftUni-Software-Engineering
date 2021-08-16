1ac05f63c9d41936da5016392aba77b67d39a3d9
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-08-16 09:52:44 +0300

    Counter Strike
    Added a new problem

---
+++ CSharp-Programming-Fundamentals/Exams/MidExams/03.MidExam/03.MidExam/01. Counter Strike/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01._Counter_Strike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int health = int.Parse(Console.ReadLine());
+            string input = string.Empty;
+            int winCounter = 0;
+            while ((input = Console.ReadLine())!="End of battle")
+            {
+                int distance = int.Parse(input);
+                if (health>=distance)
+                {
+                    winCounter++;
+                    health -= distance;
+                }
+                else
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {winCounter} won battles and {health} energy");
+                    return;
+                }
+                if (winCounter%3==0)
+                {
+                    health += winCounter;
+                }
+
+            }
+            Console.WriteLine($"Won battles: {winCounter}. Energy left: {health}");
+        }
+    }
+}
