82a48f9fc233a24b604fadda9c44ec81533f0ef6
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-08-18 11:33:01 +0300

    National Court
    Added a new task

---
+++ CSharp-Programming-Fundamentals/Exams/MidExams/04.MidExam/04.MidExam/01. National Court/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01._National_Court
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEfficiency = int.Parse(Console.ReadLine());
+            int secondEfficiency = int.Parse(Console.ReadLine());
+            int thirdEfficiency = int.Parse(Console.ReadLine());
+        }
+    }
+}
