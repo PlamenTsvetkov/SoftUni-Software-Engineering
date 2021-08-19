6aa3230629e872d6341f9412f419263925630a4c
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-08-19 17:51:53 +0300

    Bonus Scoring System
    Added a new task

---
+++ CSharp-Programming-Fundamentals/Exams/MidExams/05.MidExam/MidExam/01. Bonus Scoring System/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _01._Bonus_Scoring_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int students = int.Parse(Console.ReadLine());
+            double lectures = double.Parse(Console.ReadLine());
+            if (lectures == 0)
+            {
+                Console.WriteLine($"Max Bonus: 0.");
+                Console.WriteLine($"The student has attended 0 lectures.");
+                return;
+            }
+            int initialBonus = int.Parse(Console.ReadLine());
+            double maxBonus = int.MinValue;
+            double maxAttendance = int.MinValue;
+            for (int i = 0; i < students; i++)
+            {
+                double currentStudentsAttendance = double.Parse(Console.ReadLine());
+                double currentTotalBonus = currentStudentsAttendance / lectures * (5 + initialBonus);
+                currentTotalBonus = Math.Ceiling(currentTotalBonus);
+                if (currentTotalBonus>=maxBonus)
+                {
+                    maxBonus = currentTotalBonus;
+                    maxAttendance = currentStudentsAttendance;
+                }
+
+            }
+            Console.WriteLine($"Max Bonus: {maxBonus}.");
+            Console.WriteLine($"The student has attended {maxAttendance} lectures.");
+        }
+    }
+}
