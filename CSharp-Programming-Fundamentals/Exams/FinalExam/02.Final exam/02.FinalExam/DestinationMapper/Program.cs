ab56e1130d6625237b011cbbb6c1cd1bb619416d
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-08-21 22:26:26 +0300

    Destination mapper
    Added a new task

---
+++ CSharp-Programming-Fundamentals/Exams/FinalExam/02.Final exam/02.FinalExam/DestinationMapper/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace DestinationMapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string pattern = @"([\/=])(?<destination>[A-Z][a-zA-Z]{2,})\1";
+            var regex = Regex.Matches(text, pattern);
+            int points = 0;
+            List<string> destination = new List<string>();
+            foreach (Match match in regex)
+            {
+                points += match.Groups["destination"].Value.Length;
+                destination.Add(match.Groups["destination"].Value);
+            }
+            Console.WriteLine($"Destinations: {string.Join(", ", destination)}");
+            Console.WriteLine($"Travel Points: {points}");
+        }
+    }
+}
