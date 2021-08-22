b4a494b8a53365ca825e125e29a3aaa7dd7ca1d0
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-08-22 22:30:23 +0300

    Mirror words
    Added a new task

---
+++ CSharp-Programming-Fundamentals/Exams/FinalExam/03.Final exam/P03PFFE/P02MirrorWords/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace P02MirrorWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string textPattern = @"([@|#])(?<firstWord>[A-Za-z]{3,})\1{2}(?<secondWord>[A-Za-z]{3,})\1";
+            var regex = Regex.Matches(text, textPattern);
+            List<string> mirrorList = new List<string>();
+            if (regex.Count==0)
+            {
+                Console.WriteLine("No word pairs found!");
+            }
+            else
+            {
+                Console.WriteLine($"{regex.Count} word pairs found!");
+            }
+            foreach (Match match in regex)
+            {
+                string firstWord = match.Groups["firstWord"].Value;
+                string secondWord = match.Groups["secondWord"].Value;
+                string reverseSecondWord = ReverseString(secondWord);
+                if (firstWord==reverseSecondWord)
+                {
+                    mirrorList.Add($"{firstWord} <=> {secondWord}");
+                }
+            }
+            if (mirrorList.Count==0)
+            {
+                Console.WriteLine("No mirror words!");
+            }
+            else
+            {
+                Console.WriteLine("The mirror words are:");
+                Console.WriteLine(string.Join(", ",mirrorList));
+            }
+        }
+
+        public static string ReverseString(string secondWord)
+        {
+            char[] array = secondWord.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+    }
+}
