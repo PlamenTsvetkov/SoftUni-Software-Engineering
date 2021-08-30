c79d375e5ba8f5ce7e8a2973dd10831c0eebba59
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-08-30 10:36:40 +0300

    Activation Key
    Added a new task

---
+++ CSharp-Programming-Fundamentals/Exams/FinalExam/05.Final exam/05FinalExam/P01ActivationKey/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace P01ActivationKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string activationKey = Console.ReadLine();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input=="Generate")
+                {
+                    break;
+                }
+                string[] allCommand = input
+                    .Split(">>>", StringSplitOptions.RemoveEmptyEntries);
+                string command = allCommand[0];
+                if (command=="Contains")
+                {
+                    string substring = allCommand[1];
+                    if (activationKey.Contains(substring))
+                    {
+                        Console.WriteLine($"{activationKey} contains {substring}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Substring not found!");
+                    }
+                }
+                else if (command=="Flip")
+                {
+                    string typeCase = allCommand[1];
+                    int startIndex = int.Parse(allCommand[2]);
+                    int endIndex = int.Parse(allCommand[3]);
+                    int length = endIndex - startIndex ;
+                    if (typeCase=="Upper")
+                    {
+
+                        string substring = activationKey.Substring(startIndex, length);
+                        activationKey = activationKey.Replace(substring, substring.ToString().ToUpper()); 
+                    }
+                    else if (typeCase=="Lower")
+                    {
+                        string substring = activationKey.Substring(startIndex, length);
+                        activationKey = activationKey.Replace(substring, substring.ToString().ToLower());
+                    }
+                    Console.WriteLine(activationKey);
+                }
+                else if (command=="Slice")
+                {
+                    int startIndex = int.Parse(allCommand[1]);
+                    int endIndex = int.Parse(allCommand[2]);
+                    int length = endIndex - startIndex;
+                    activationKey = activationKey.Remove(startIndex, length);
+                    Console.WriteLine(activationKey);
+                }
+            }
+            Console.WriteLine($"Your activation key is: {activationKey}");
+        }
+    }
+}
