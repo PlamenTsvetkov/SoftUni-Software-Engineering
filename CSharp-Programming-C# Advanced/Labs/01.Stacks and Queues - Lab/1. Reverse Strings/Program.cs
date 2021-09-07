0d42e0a413049f0cd7b431354ce59df6510f342e
PlamenTsvetkov <cvetkov_finance@abv.bg>
2021-09-07 16:05:32 +0300

    Added a new task
    Reverse Strings

---
+++ CSharp-Programming-C# Advanced/Labs/01.Stacks and Queues - Lab/1. Reverse Strings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string text = Console.ReadLine();
+            //var reverseText = new Stack<char>(text);
+            var reverseText = new Stack<char>(Console.ReadLine());
+            while (reverseText.Any())
+            {
+                Console.Write(reverseText.Pop());
+            }
+        }
+    }
+}
